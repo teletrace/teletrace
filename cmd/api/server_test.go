b2ce07c4c7e5953fe0f30aba3845c24cd4290df8
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-03 00:06:56 +0300

    Replace Gin default logger with Zap

--- cmd/api/server_test.go
+++ cmd/api/server_test.go
@@ -6,15 +6,28 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
 )
 
 func TestPingRoute(t *testing.T) {
-	router := setupRouter()
+	pingRoute := "/v1/ping"
 
+	zapCore, observedLogs := observer.New(zap.InfoLevel)
+	fakeLogger := zap.New(zapCore)
+
+	req, _ := http.NewRequest(http.MethodGet, pingRoute, nil)
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/v1/ping", nil)
+
+	router := setupRouter(fakeLogger)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+
+	logs := observedLogs.All()
+	assert.Equal(t, 1, len(logs))
+	logFields := logs[0].ContextMap()
+	assert.Equal(t, pingRoute, logFields["path"])
+	assert.Equal(t, http.MethodGet, logFields["method"])
 }
