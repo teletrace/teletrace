84c1dba9cafe3cacab3ef8abcdf0261eac7c243e
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-03 01:15:37 +0300

    Separate logger testing from route testing

--- cmd/api/server_test.go
+++ cmd/api/server_test.go
@@ -11,23 +11,35 @@ import (
 )
 
 func TestPingRoute(t *testing.T) {
-	pingRoute := "/v1/ping"
-
-	zapCore, observedLogs := observer.New(zap.InfoLevel)
-	fakeLogger := zap.New(zapCore)
-
-	req, _ := http.NewRequest(http.MethodGet, pingRoute, nil)
-	w := httptest.NewRecorder()
+	fakeLogger, _ := getLoggerObserver()
+	req, _ := http.NewRequest(http.MethodGet, "/v1/ping", nil)
+	resRecorder := httptest.NewRecorder()
 
 	router := setupRouter(fakeLogger)
-	router.ServeHTTP(w, req)
+	router.ServeHTTP(resRecorder, req)
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
+	assert.Equal(t, http.StatusOK, resRecorder.Code)
+	assert.Equal(t, "pong", resRecorder.Body.String())
+}
+
+func TestLoggerMiddleware(t *testing.T) {
+	pingRoute := "/v1/ping"
+	fakeLogger, observedLogs := getLoggerObserver()
+	req, _ := http.NewRequest(http.MethodGet, pingRoute, nil)
+	resRecorder := httptest.NewRecorder()
+
+	router := setupRouter(fakeLogger)
+	router.ServeHTTP(resRecorder, req)
 
 	logs := observedLogs.All()
 	assert.Equal(t, 1, len(logs))
 	logFields := logs[0].ContextMap()
 	assert.Equal(t, pingRoute, logFields["path"])
 	assert.Equal(t, http.MethodGet, logFields["method"])
+}
+
+func getLoggerObserver() (*zap.Logger, *observer.ObservedLogs) {
+	zapCore, observedLogs := observer.New(zap.InfoLevel)
+	fakeLogger := zap.New(zapCore)
+	return fakeLogger, observedLogs
 }
