0c590cd48b22fdf931d7f3aaf127ab6622e70272
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-03 11:10:37 +0300

    Add recovery logger test

--- cmd/api/server_test.go
+++ cmd/api/server_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest/observer"
@@ -36,6 +37,33 @@ func TestLoggerMiddleware(t *testing.T) {
 	logFields := logs[0].ContextMap()
 	assert.Equal(t, pingRoute, logFields["path"])
 	assert.Equal(t, http.MethodGet, logFields["method"])
+}
+
+func TestRecoveryLoggerMiddleware(t *testing.T) {
+	panicRoute := "/panic-test-route"
+	panicMsg := "this is a test panic"
+	fakeLogger, observedLogs := getLoggerObserver()
+	req, _ := http.NewRequest(http.MethodGet, panicRoute, nil)
+	resRecorder := httptest.NewRecorder()
+
+	router := setupRouter(fakeLogger)
+	router.GET(panicRoute, func(c *gin.Context) {
+		panic(panicMsg)
+	})
+	router.ServeHTTP(resRecorder, req)
+
+	assert.Equal(t, 2, observedLogs.Len())
+
+	recoveryLog := observedLogs.FilterMessage("[Recovery from panic]")
+	assert.Equal(t, 1, recoveryLog.Len())
+	recoveryLogFields := recoveryLog.All()[0].ContextMap()
+	assert.Equal(t, panicMsg, recoveryLogFields["error"])
+
+	routeLog := observedLogs.FilterMessage(panicRoute)
+	assert.Equal(t, 1, routeLog.Len())
+	routeLogFields := routeLog.All()[0].ContextMap()
+	assert.Equal(t, panicRoute, routeLogFields["path"])
+	assert.Equal(t, int64(500), routeLogFields["status"])
 }
 
 func getLoggerObserver() (*zap.Logger, *observer.ObservedLogs) {
