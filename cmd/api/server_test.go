b6568e8ac0a873ff2451eaf471f6479db5233dfe
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-04 23:06:43 +0300

    feat(api): Replace Gin default logger with Zap (#61)
    **What this PR does**:
    Replaces Gin default logger with the Zap logger.
    Each request to the API will be logged with some relevant fields:
    `{"level":"info","ts":1662153040.778379,"caller":"zap@v0.0.2/zap.go:77","msg":"/v1/ping","status":200,"method":"GET","path":"/v1/ping","query":"","ip":"::1","user-agent":"PostmanRuntime/7.29.2","latency":0.000044273,"time":"2022-09-02T21:10:40Z"}`
    
    Do not get confused with https://github.com/epsagon/oss-tracing/issues/60 which will be handled in a different PR.
    
    **Which issue(s) this PR fixes**:
    Fixes #56 
    
    **Checklist**
    - [x] Tests updated
    - [ ] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

--- cmd/api/server_test.go
+++ cmd/api/server_test.go
@@ -5,16 +5,69 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
 )
 
 func TestPingRoute(t *testing.T) {
-	router := setupRouter()
-
-	w := httptest.NewRecorder()
+	fakeLogger, _ := getLoggerObserver()
 	req, _ := http.NewRequest(http.MethodGet, "/v1/ping", nil)
-	router.ServeHTTP(w, req)
+	resRecorder := httptest.NewRecorder()
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
+	router := setupRouter(fakeLogger)
+	router.ServeHTTP(resRecorder, req)
+
+	assert.Equal(t, http.StatusOK, resRecorder.Code)
+	assert.Equal(t, "pong", resRecorder.Body.String())
+}
+
+func TestLoggerMiddleware(t *testing.T) {
+	pingRoute := "/v1/ping"
+	fakeLogger, observedLogs := getLoggerObserver()
+	req, _ := http.NewRequest(http.MethodGet, pingRoute, nil)
+	resRecorder := httptest.NewRecorder()
+
+	router := setupRouter(fakeLogger)
+	router.ServeHTTP(resRecorder, req)
+
+	logs := observedLogs.All()
+	assert.Equal(t, 1, len(logs))
+	logFields := logs[0].ContextMap()
+	assert.Equal(t, pingRoute, logFields["path"])
+	assert.Equal(t, http.MethodGet, logFields["method"])
+}
+
+func TestRecoveryLoggerMiddleware(t *testing.T) {
+	panicRoute := "/panic-test-route"
+	panicMsg := "this is a test panic"
+	fakeLogger, observedLogs := getLoggerObserver()
+	req, _ := http.NewRequest(http.MethodGet, panicRoute, nil)
+	resRecorder := httptest.NewRecorder()
+
+	router := setupRouter(fakeLogger)
+	router.GET(panicRoute, func(c *gin.Context) {
+		panic(panicMsg)
+	})
+	router.ServeHTTP(resRecorder, req)
+
+	assert.Equal(t, 2, observedLogs.Len())
+
+	recoveryLog := observedLogs.FilterMessage("[Recovery from panic]")
+	assert.Equal(t, 1, recoveryLog.Len())
+	recoveryLogFields := recoveryLog.All()[0].ContextMap()
+	assert.Equal(t, panicMsg, recoveryLogFields["error"])
+
+	routeLog := observedLogs.FilterMessage(panicRoute)
+	assert.Equal(t, 1, routeLog.Len())
+	routeLogFields := routeLog.All()[0].ContextMap()
+	assert.Equal(t, panicRoute, routeLogFields["path"])
+	assert.Equal(t, int64(500), routeLogFields["status"])
+}
+
+func getLoggerObserver() (*zap.Logger, *observer.ObservedLogs) {
+	zapCore, observedLogs := observer.New(zap.InfoLevel)
+	fakeLogger := zap.New(zapCore)
+	return fakeLogger, observedLogs
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module oss-tracing
 go 1.19
 
 require (
+	github.com/gin-contrib/zap v0.0.2
 	github.com/gin-gonic/gin v1.8.1
 	github.com/spf13/viper v1.12.0
 	github.com/stretchr/testify v1.7.1
-	go.uber.org/zap v1.17.0
+	go.uber.org/zap v1.19.1
 )
 
 require (
