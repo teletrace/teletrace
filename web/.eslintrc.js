cc20493463d2f1ed3f79fcc445cf4330431416a2
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-12 10:33:53 +0300

    fix: eslint react version related config (#116)
    **What this PR does**:
    1. Specifies the React version for plugins to use. This removes the "version not specified" error:
    ![Screen Shot 2022-09-12 at 9 28 30](https://user-images.githubusercontent.com/37577482/189588427-6ecba945-4715-47e1-b4e8-ffc2c9ba00bd.png)
    
    2. In newer React versions, React doesn't have to be in the scope of JSX components thanks to JSX transform. The current eslint config requires this unnecessary import, which also leads to the IDE marking the import as unused.
    ![Screen Shot 2022-09-12 at 9 28 44](https://user-images.githubusercontent.com/37577482/189589900-add5c224-bcac-435f-bf04-d8e24bc9d3b8.png)
    More info here:
    https://github.com/jsx-eslint/eslint-plugin-react#configuration

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  },
+  rules: {},
+};
