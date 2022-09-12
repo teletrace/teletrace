36c6b2ecf8d7d791fcfb84b4aeaab8883e1a188f
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-12 11:05:27 +0300

    feat(ui): add basic react router (#118)
    **What this PR does**:
    Adds basic routing to the frontend application.
    
    ![Screen Shot 2022-09-12 at 10 43 43](https://user-images.githubusercontent.com/37577482/189599505-b7112298-a49a-42ed-98da-f9b63cd55e1e.png)
    
    **Which issue(s) this PR fixes**:
    Fixes #72

--- web/src/components/core/App/index.tsx
+++ web/src/components/core/App/index.tsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Layout } from "../Layout";
+
 export function App() {
-  return <div>OSS Tracing App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h1>Homepage</h1>} />
+        <Route path="traces" element={<h1>Traces</h1>} />
+      </Route>
+    </Routes>
+  );
 }
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./components/core/App";
 
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
