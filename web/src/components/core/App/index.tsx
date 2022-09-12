50a7e545d5e81eb1dda82b8294c3e5f6e3efe6fa
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-09-12 10:36:18 +0300

    Remove unused react imports

--- web/src/components/core/App/index.tsx
+++ web/src/components/core/App/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Layout } from "../Layout";
--- web/src/components/core/Layout/index.tsx
+++ web/src/components/core/Layout/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export function Layout() {
