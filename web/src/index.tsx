38527e79c82273c8ae73e02afc82132533399d84
shaqued <shaqued201@gmail.com>
2022-09-18 20:14:50 +0300

    remove redundent

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
