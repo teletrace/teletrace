8b3e1ab19fb5861aa9747eb34e9e325da8acfcf3
shaqued <shaqued201@gmail.com>
2022-09-18 17:20:30 +0300

    feat: add react query devtools

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { App } from "./components/core/App";
 
@@ -17,6 +18,7 @@ root.render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
