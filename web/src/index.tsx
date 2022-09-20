7f4151926ae6c966a35be3f3236c972a85542aab
tvaintrob <vintrob@cisco.com>
2022-09-20 17:02:29 +0300

    ci: pre-commit fixes

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
 
@@ -10,12 +10,12 @@ const queryClient = new QueryClient();
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
