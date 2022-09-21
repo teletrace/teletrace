548f11a8a7dbc0b97e3a40accf4853190910faf2
Tal Vintrob <tvaintrob@users.noreply.github.com>
2022-09-21 13:28:20 +0300

    feat: frontend folder structure (#154)
    **What this PR does**:
    formalize frontend directory structure, and add some `eslint` rules for validation.
    structure is based on [Bulletproof React](https://github.com/alan2207/bulletproof-react)
    
    **Which issue(s) this PR fixes**:
    Fixes #75 
    
    **Checklist**
    
    - [ ] Tests updated
    - [x] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,24 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import { App } from "./components/core/App";
+import { App } from "./App";
 
 const queryClient = new QueryClient();
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
