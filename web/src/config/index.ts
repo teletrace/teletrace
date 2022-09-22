597a78b28033b84d9907871a3a5853ef6a3c94f8
Haim Raitsev <haimrait@gmail.com>
2022-09-22 17:25:50 +0300

    fix: cr changes

--- web/src/config/index.ts
+++ web/src/config/index.ts
@@ -1,3 +1,2 @@
 // this file contains configuration for the application such as API_URL, etc.
-export const API_URL =
-  (process.env.API_URL as string) || "http://localhost:8000";
+export const API_URL = process.env.API_URL || "http://localhost:8000";
