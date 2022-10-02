75400c226ad18875ebfbd556ecb26b79e89536dd
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-10-02 16:17:09 +0300

    Add grpc endpoint test

--- pkg/receiver/receiver_test.go
+++ pkg/receiver/receiver_test.go
@@ -10,25 +10,27 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	otelcfg "go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
 	"go.opentelemetry.io/collector/receiver/otlpreceiver"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest/observer"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func TestReceiverTracesProcessor(t *testing.T) {
-	cfg := config.Config{
-		GRPCEndpoint: "0.0.0.0:1234",
-		HTTPEndpoint: "0.0.0.0:4321",
-	}
+func TestReceiverGRPCEndpoint(t *testing.T) {
+	fakeSpanName := "fakeSpan"
+	receivedSpanLog := "Received test span"
+	cfg := config.Config{GRPCEndpoint: "0.0.0.0:1234"}
 	logger, observedLogs := getLoggerObserver()
 	tracesProcessor := func(ctx context.Context, processorLogger *zap.Logger, td ptrace.Traces) error {
-		processorLogger.Info("Received traces")
 		receivedSpan := td.ResourceSpans().At(0).ScopeSpans().At(0).Spans().At(0)
-		assert.Equal(t, "fakeSpan", receivedSpan.Name())
+		processorLogger.Info(receivedSpanLog, zap.String("span_name", receivedSpan.Name()))
 		return nil
 	}
 
@@ -39,7 +41,64 @@ func TestReceiverTracesProcessor(t *testing.T) {
 		assert.NoError(t, receiver.Shutdown())
 	}()
 
-	reqBody := strings.NewReader(`
+	grpcClient, err := grpc.Dial(
+		cfg.GRPCEndpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	require.NoError(t, err)
+	defer func() {
+		assert.NoError(t, grpcClient.Close())
+	}()
+	td := createTracesTestSpan(fakeSpanName)
+	require.NoError(t, exportTraces(grpcClient, td))
+
+	processorLog := observedLogs.FilterMessage(receivedSpanLog)
+	assert.Equal(t, 1, processorLog.Len())
+	processorLogFields := processorLog.All()[0].ContextMap()
+	assert.Equal(t, fakeSpanName, processorLogFields["span_name"])
+}
+
+func getLoggerObserver() (*zap.Logger, *observer.ObservedLogs) {
+	zapCore, observedLogs := observer.New(zap.InfoLevel)
+	// WithFatalHook is used to avoid exiting during tests
+	fakeLogger := zap.New(zapCore, zap.WithFatalHook(zapcore.WriteThenPanic))
+	return fakeLogger, observedLogs
+}
+
+func createTracesTestSpan(spanName string) ptrace.Traces {
+	traces := ptrace.NewTraces()
+	span := traces.ResourceSpans().AppendEmpty().ScopeSpans().AppendEmpty().Spans().AppendEmpty()
+	span.SetName(spanName)
+	return traces
+}
+
+func exportTraces(grpcClient *grpc.ClientConn, td ptrace.Traces) error {
+	otlpClient := ptraceotlp.NewClient(grpcClient)
+	req := ptraceotlp.NewRequestFromTraces(td)
+	_, err := otlpClient.Export(context.Background(), req)
+	return err
+}
+
+func TestReceiverHTTPEndpoint(t *testing.T) {
+	fakeSpanName := "fakeSpan"
+	receivedSpanLog := "Received test span"
+	cfg := config.Config{HTTPEndpoint: "0.0.0.0:4321"}
+	logger, observedLogs := getLoggerObserver()
+	tracesProcessor := func(ctx context.Context, processorLogger *zap.Logger, td ptrace.Traces) error {
+		receivedSpan := td.ResourceSpans().At(0).ScopeSpans().At(0).Spans().At(0)
+		processorLogger.Info(receivedSpanLog, zap.String("span_name", receivedSpan.Name()))
+		return nil
+	}
+
+	receiver, err := NewReceiver(cfg, logger, tracesProcessor)
+	assert.NoError(t, err)
+	assert.NoError(t, receiver.Start())
+	defer func() {
+		assert.NoError(t, receiver.Shutdown())
+	}()
+
+	reqBody := strings.NewReader(fmt.Sprintf(`
 	{
 		"resource_spans": [
 		  {
@@ -47,7 +106,7 @@ func TestReceiverTracesProcessor(t *testing.T) {
 			  {
 				"spans": [
 				  {
-					"name": "fakeSpan"
+					"name": "%s"
 				  }
 				]
 			  }
@@ -55,12 +114,14 @@ func TestReceiverTracesProcessor(t *testing.T) {
 		  }
 		]
 	  }
-	`)
+	`, fakeSpanName))
 	resp, _ := http.Post(fmt.Sprintf("http://%s/v1/traces", cfg.HTTPEndpoint), "application/json", reqBody)
 	assert.Equal(t, 200, resp.StatusCode)
-	// Apart from ensuring processor invocation, it also validates the correct logger is used
-	processorLog := observedLogs.FilterMessage("Received traces")
+
+	processorLog := observedLogs.FilterMessage(receivedSpanLog)
 	assert.Equal(t, 1, processorLog.Len())
+	processorLogFields := processorLog.All()[0].ContextMap()
+	assert.Equal(t, fakeSpanName, processorLogFields["span_name"])
 }
 
 func TestReceiverConfig(t *testing.T) {
@@ -96,10 +157,4 @@ func TestOtelHostReportFatalError(t *testing.T) {
 	assert.Equal(t, "OTLP receiver error", log.Entry.Message)
 	assert.Equal(t, zap.FatalLevel, log.Entry.Level)
 	assert.Equal(t, "Example error", log.ContextMap()["error"])
-}
-
-func getLoggerObserver() (*zap.Logger, *observer.ObservedLogs) {
-	zapCore, observedLogs := observer.New(zap.InfoLevel)
-	fakeLogger := zap.New(zapCore, zap.WithFatalHook(zapcore.WriteThenPanic))
-	return fakeLogger, observedLogs
 }
