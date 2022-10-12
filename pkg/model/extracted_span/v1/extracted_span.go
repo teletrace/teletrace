a7c64bbc313937fb5a2058a937a6bb33624276eb
Roy Ben Haroosh <roybh@cisco.com>
2022-10-12 13:02:02 +0300

    feat: add span query layer request and response structures

---
+++ pkg/model/extracted_span/v1/extracted_span.go
@@ -0,0 +1,65 @@
+package v1
+
+type Attributes map[string]any
+
+type SpanEvent struct {
+	TimeUnixNano           uint64
+	Name                   string
+	Attributes             Attributes
+	DroppedAttributesCount uint32
+}
+
+type SpanLink struct {
+	TraceId                []byte
+	SpanId                 []byte
+	TraceState             string
+	Attributes             Attributes
+	DroppedAttributesCount uint32
+}
+
+type SpanStatus struct {
+	Message string
+	Code    uint32
+}
+
+type Resource struct {
+	Attributes             Attributes
+	DroppedAttributesCount int32
+}
+
+type InstrumentationScope struct {
+	Name                   string
+	Version                string
+	Attributes             Attributes
+	DroppedAttributesCount int32
+}
+
+type Span struct {
+	TraceId                []byte
+	SpanId                 []byte
+	TraceState             string
+	ParentSpanId           []byte
+	Name                   string
+	Kind                   int32
+	StartTimeUnixNano      uint64
+	EndTimeUnixNano        uint64
+	Attributes             Attributes
+	DroppedAttributesCount int32
+	Events                 []*SpanEvent
+	DroppedEventsCount     uint32
+	Links                  []*SpanLink
+	DroppedLinksCount      uint32
+	Status                 *SpanStatus
+}
+
+type ExternalFields struct {
+	DurationNano uint64
+}
+
+type ExtractedSpan struct {
+	Resource       *Resource
+	Scope          *InstrumentationScope
+	Span           *Span
+	ExternalFields *ExternalFields
+	TimestampNano  uint64
+}
---
+++ pkg/model/spans_query/v1/spans_query.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	v1 "oss-tracing/pkg/model/extracted_span/v1"
+	"time"
+)
+
+type Timeframe struct {
+	StartTime time.Time
+	EndTime   time.Time
+}
+
+type SortField struct {
+	Field     string
+	Direction int
+}
+
+type SearchFilter struct {
+	Key         string
+	Operator    string
+	Value       any
+	QueryString string
+}
+
+type RequestMetadata struct {
+	NextToken string
+}
+
+type SearchRequest struct {
+	Timeframe       *Timeframe
+	SortField       *SortField
+	SearchFilter    []*SearchFilter
+	RequestMetadata *RequestMetadata
+}
+
+type ResponseMetadata struct {
+	NextToken string
+}
+
+type SearchResponse struct {
+	ResponseMetadata *ResponseMetadata
+	Spans            []*v1.ExtractedSpan
+}
