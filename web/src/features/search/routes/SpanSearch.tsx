eada0c12c671e43ec4226273d8f0a674c7e1fe9f
Tav <therzlic@cisco.com>
2022-10-18 18:00:49 +0300

    Added SpanTable component and route

---
+++ web/src/features/search/routes/SpanSearch.tsx
@@ -0,0 +1,42 @@
+import { Divider } from "@mui/material";
+import { Stack } from "@mui/system";
+
+import { Head } from "@/components/Head";
+
+import { SpanTable } from "../components/SpanTable";
+
+export const SpanSearch = () => {
+  return (
+    <>
+      <Head
+        title="Span Search"
+        description="Designated page to span search's flow graph and timeline"
+      />
+      <Stack
+        direction="column"
+        divider={<Divider orientation="horizontal" flexItem />}
+        spacing={2}
+        sx={{ height: "100%" }}
+      >
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+          spacing={2}
+          justifyContent="space-between"
+          flex={1}
+        >
+          <SpanTable />
+        </Stack>
+
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+          spacing={2}
+          flex={1}
+        >
+
+        </Stack>
+      </Stack>
+    </>
+  );
+};
