e3f21a30ff8fd48a65e4cfbe3f833f9f252f49c6
Tal Vintrob <tvaintrob@users.noreply.github.com>
2022-11-02 13:17:08 +0200

    feat: added spans-query types (#257)
    **What this PR does**:
    
    adds type needed to fetch spans from the backend

---
+++ web/src/features/search/api/spanQuery.ts
@@ -0,0 +1,18 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { SearchRequest, SearchResponse } from "../types/spanQuery";
+
+type FetchSpansParams = { pageParam: string; searchRequest: SearchRequest };
+
+export const fetchSpans = ({ pageParam, searchRequest }: FetchSpansParams) => {
+  searchRequest.metadata = { nextToken: pageParam };
+  return Promise.reject<SearchResponse>("not implemented");
+};
+
+export const useSpansQuery = (searchRequest: SearchRequest) => {
+  return useInfiniteQuery({
+    queryKey: ["spans", searchRequest],
+    queryFn: ({ pageParam }) => fetchSpans({ pageParam, searchRequest }),
+    getNextPageParam: (lastPage) => lastPage?.metadata?.nextToken,
+  });
+};
