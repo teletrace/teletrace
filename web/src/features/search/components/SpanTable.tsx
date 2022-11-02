8fbc278fc32ee0e1574cdec7b9e90684da2ac402
shaqued <shaqued201@gmail.com>
2022-11-02 11:20:01 +0200

    add seleton for search bar and "add filter" button

--- web/src/features/search/components/SpanTable.tsx
+++ web/src/features/search/components/SpanTable.tsx
@@ -2,7 +2,7 @@ import { Paper, Typography } from "@mui/material";
 
 export const SpanTable = () => {
   return (
-    <Paper sx={{ width: "100%" }}>
+    <Paper sx={{ width: "100%", height: "100%" }}>
       <Typography>Span table goes here</Typography>
     </Paper>
   );
