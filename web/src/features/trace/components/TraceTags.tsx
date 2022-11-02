b6170632cd17f8bf5b180f747b0105705fcbdd8a
Reut Baruch <reutbar@cisco.com>
2022-11-02 15:35:50 +0200

    add main accordion design

--- web/src/features/trace/components/TraceTags.tsx
+++ web/src/features/trace/components/TraceTags.tsx
@@ -1,4 +1,4 @@
-import DeleteIcon from "@mui/icons-material/Delete";
+import ArrowIcon from "@mui/icons-material/ArrowForward";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -6,81 +6,102 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 import styles from "../styles/styles.tags";
+import { SvgIcon } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { padding } from "@mui/system";
+
+interface TagsProps {
+  spanAction: string;
+  selectedSpanId: string;
+  spanDuration: string;
+  spanDateTime: string;
+}
 
 export const TraceTags = () => {
-  const title = `Execute`;
-  const body = `192.25ms | 1:36:12.610 - Jan 24, 2022`;
+  const [expanded, setExpanded] = useState<string | false>(false);
 
-  function TagsSubAccordion() {
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
+
+  function TagsMainAccordion(props: TagsProps) {
     return (
-      <Accordion>
+      <Accordion
+        expanded={expanded === props.selectedSpanId}
+        onChange={handleChange(props.selectedSpanId)}
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           sx={{ flexDirection: "row-reverse" }}
+          style={{
+            backgroundColor: expanded === props.selectedSpanId ? grey[800] : "",
+          }}
         >
-          <Typography
-            variant="subtitle1"
-            sx={{ fontWeight: "bold", paddingLeft: 1 }}
-          >
-            Basic
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Grid
-            container
-            sx={styles.accordionGridContainer}
-            spacing={2}
-            columns={16}
-          >
-            <Grid item sx={styles.accordionGridItem} xs={8} textAlign="center">
-              <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-                {title}
-              </Typography>
+          <Grid container justifyContent="flex-start" alignItems="center">
+            <Grid
+              item
+              sx={{
+                flexDirection: "column",
+                paddingLeft: 1.5,
+                paddingRight: 1.5,
+              }}
+              style={{
+                display: "center",
+                alignSelf: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <SvgIcon
+                sx={{ fontSize: 30 }}
+                style={{ fill: "none", stroke: "#3F8624" }}
+              >
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+              </SvgIcon>
+              <ArrowIcon
+                sx={{
+                  color: "#6E6F75",
+                  fontSize: "medium",
+                }}
+              />
+              <SvgIcon
+                sx={{ fontSize: 30 }}
+                style={{ fill: "none", stroke: "#D45B07" }}
+              >
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+              </SvgIcon>
             </Grid>
-            <Grid sx={styles.accordionGridItem} xs={8}>
-              <Typography variant="caption">{body}</Typography>
-            </Grid>
-          </Grid>
-        </AccordionDetails>
-      </Accordion>
-    );
-  }
-
-  function TagsMainAccordion() {
-    return (
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          sx={{ flexDirection: "row-reverse" }}
-        >
-          <DeleteIcon sx={{ alignSelf: "center" }} />
-          <Grid container sx={styles.accordionGridContainer}>
-            <Grid item sx={styles.accordionGridItem}>
+            <Grid
+              item
+              sx={{
+                alignItems: "flex-start",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
               <Typography sx={styles.accordionTitleTypography}>
-                {title}
+                {props.spanAction}
               </Typography>
-            </Grid>
-            <Grid item width="100%" sx={styles.accordionGridItem}>
-              <Typography sx={styles.accordionBodyTypography}>
-                {body}
+              <Typography variant="caption" sx={{ color: grey[300] }}>
+                {`${props.spanDuration}ms`} | {props.spanDateTime}
               </Typography>
             </Grid>
           </Grid>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            <TagsSubAccordion />
-          </Typography>
+          <Typography>Add inner accordion</Typography>
         </AccordionDetails>
       </Accordion>
     );
   }
 
   return (
-    <Paper sx={{ width: "100%" }}>
-      <TagsMainAccordion />
-      <TagsMainAccordion />
+    <Paper sx={{ width: "100%"}} >
+      <TagsMainAccordion selectedSpanId="panel1" spanAction="Execute" spanDuration="192.45" spanDateTime="1:36.12.610 - Jan 24, 2022" />
+      <TagsMainAccordion selectedSpanId="panel2" spanAction="Execute" spanDuration="192.45" spanDateTime="1:36.12.610 - Jan 24, 2022" />
     </Paper>
   );
 };
--- web/src/features/trace/styles/styles.tags.tsx
+++ web/src/features/trace/styles/styles.tags.tsx
@@ -1,30 +1,10 @@
 const styles = {
-  accordionGridContainer: {
-    direction: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    paddingLeft: 1,
-  },
-  accordionGridItem: {
-    alignItems: "flex-start",
-    display: "flex",
-    flexDirection: "column",
-  },
   accordionTitleTypography: {
     fontFamily: "Inter",
     fontWeight: "700",
     fontSize: "14px",
     color: "#FFFFFF",
-    display: "flex",
-    alignItems: "center",
-  },
-  accordionBodyTypography: {
-    fontFamily: "Inter",
-    fontWeight: "500",
-    fontSize: "12px",
-    color: "#B6B7BE",
-    display: "flex",
-    alignItems: "center",
+    paddingBottom: 1,
   },
 };
 
