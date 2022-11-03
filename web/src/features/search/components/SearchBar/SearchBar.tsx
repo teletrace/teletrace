db0ba4f7d0dca4f60228b5814620753c33e0b1c5
shaqued <shaqued201@gmail.com>
2022-11-03 16:03:18 +0200

    wip

--- web/src/features/search/components/SearchBar/SearchBar.tsx
+++ web/src/features/search/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,31 @@
 import { FilterList } from "@mui/icons-material";
 import { Button, Paper } from "@mui/material";
+import { useState } from "react";
+
+import { AddFilterDialog } from "../AddFilter";
 
 export function SearchBar() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Paper sx={{ height: "40px", padding: "8px" }}>
-      <Button variant="contained" size="small" startIcon={<FilterList />}>
+      <Button
+        variant="contained"
+        size="small"
+        startIcon={<FilterList />}
+        onClick={handleClickOpen}
+      >
         Add Filter
       </Button>
+      <AddFilterDialog open={open} onClose={handleClose} />
     </Paper>
   );
 }
