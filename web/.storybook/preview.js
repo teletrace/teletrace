209aeb26ab4a8368a78a3aa97b153d4c23119420
Nir Feinblat <nirf@cisco.com>
2022-11-06 15:38:06 +0200

    PR-237: add storybook

---
+++ web/.storybook/preview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppProviders } from '../src/providers/app';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <AppProviders>
+      <Story />
+    </AppProviders>
+  ),
+];
