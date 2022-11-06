1336a86a4a6a8249486cf379bf7ef2d423f05a96
Tav Herzlich <44731477+Tavh@users.noreply.github.com>
2022-11-06 12:15:59 +0200

    feat: added span-table component (#233)
    **What this PR does**:
    - Implements the SpanTable component using MaterialReactTable and React Query
    - Adds several new dependencies used by the SpanTable component
    
    ![Screen Shot 2022-10-25 at 16 16 05](https://user-images.githubusercontent.com/44731477/197783336-aa1f1bba-cda6-4b3f-91bc-eaf1d11516b9.png)
    
    ** The backend used to build this feature is a test gorilla/mux server, the http endpoint signature is not necessarily the one we will want in the actual server
    
    **Which issue(s) this PR fixes**:
    Fixes #100
    
    **Checklist**
    
    - [ ] Tests updated
    - [ ] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

--- web/src/libs/react-query.ts
+++ web/src/libs/react-query.ts
@@ -1,4 +1,19 @@
-import { DefaultOptions, QueryClient } from "@tanstack/react-query";
+import {
+  DefaultOptions,
+  QueryClient,
+  UseQueryOptions,
+} from "@tanstack/react-query";
+import { PromiseValue } from "type-fest";
 
 const queryConfig: DefaultOptions = {};
 export const queryClient = new QueryClient({ defaultOptions: queryConfig });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ExtractFnReturnType<FnType extends (...args: any) => any> =
+  PromiseValue<ReturnType<FnType>>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type QueryConfig<QueryFnType extends (...args: any) => any> = Omit<
+  UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
+  "queryKey" | "queryFn"
+>;
--- web/src/utils/format.ts
+++ web/src/utils/format.ts
@@ -2,3 +2,6 @@ import format from "date-fns/format";
 
 export const formatDate = (date: number) =>
   format(date, "yyyy-MM-dd'T'HH:mm:ss");
+
+export const formatDateToTimeString = (date: number) =>
+  format(date, "PP, HH:mm:ss");
