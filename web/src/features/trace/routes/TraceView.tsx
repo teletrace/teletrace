c7498e8d6913e510b3228e38d7717f9d70e6f14a
Timur Misharin <98012495+tmishari@users.noreply.github.com>
2022-11-06 15:34:09 +0200

    feat: add trace graph component (#235)
    **What this PR does**:
    Added UI component of Trace View graph.
    **Which issue(s) this PR fixes**:
    Fixes #150

--- web/src/features/trace/routes/TraceView.tsx
+++ web/src/features/trace/routes/TraceView.tsx
@@ -1,13 +1,127 @@
 import { Divider } from "@mui/material";
 import { Stack } from "@mui/system";
+import { useEffect, useState } from "react";
+import {
+  Edge,
+  MarkerType,
+  Node,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
 
+import { EdgeData, NodeData } from "@/components/Graph/types";
+import {
+  BASIC_EDGE_TYPE,
+  BASIC_NODE_TYPE,
+  EDGE_ARROW_SIZE,
+  POSITION,
+} from "@/components/Graph/utils/global";
+import { createGraphLayout } from "@/components/Graph/utils/layout";
 import { Head } from "@/components/Head";
 
 import { TraceGraph } from "../components/TraceGraph";
 import { TraceTags } from "../components/TraceTags";
 import { TraceTimeline } from "../components/TraceTimeline";
 
+const initialNodes: Node<NodeData>[] = [
+  {
+    id: "1",
+    type: BASIC_NODE_TYPE,
+    data: {
+      name: "/checkout",
+      image: "IoTHTTP2Protocol",
+      type: "Http",
+    },
+    position: POSITION,
+  },
+  {
+    id: "2",
+    type: BASIC_NODE_TYPE,
+    data: {
+      name: "/invoices",
+      image: "LambdaFunction",
+      type: "Node.js",
+    },
+    position: POSITION,
+  },
+  {
+    id: "3",
+    type: BASIC_NODE_TYPE,
+    data: {
+      name: "update-subscription",
+      image: "ApiGatewayEndpoint",
+      type: "SNS",
+    },
+    position: POSITION,
+  },
+  {
+    id: "4",
+    type: BASIC_NODE_TYPE,
+    data: {
+      name: "/payment",
+      image: "IoTHTTP2Protocol",
+      type: "Http",
+    },
+    position: POSITION,
+  },
+];
+
+const initialEdges: Edge<EdgeData>[] = [
+  {
+    id: "e1-2",
+    type: BASIC_EDGE_TYPE,
+    source: "1",
+    target: "2",
+    data: { time: "20ms", count: 2 },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: EDGE_ARROW_SIZE,
+      height: EDGE_ARROW_SIZE,
+    },
+  },
+  {
+    id: "e2-3",
+    type: BASIC_EDGE_TYPE,
+    source: "2",
+    target: "3",
+    data: { time: "20ms" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: EDGE_ARROW_SIZE,
+      height: EDGE_ARROW_SIZE,
+    },
+  },
+  {
+    id: "e2-4",
+    type: BASIC_EDGE_TYPE,
+    source: "2",
+    target: "4",
+    data: { time: "20ms" },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: EDGE_ARROW_SIZE,
+      height: EDGE_ARROW_SIZE,
+    },
+  },
+];
+
 export const TraceView = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      createGraphLayout(initialNodes, initialEdges)
+        .then((els) => {
+          if (els.nodes.length > 0) {
+            setIsLoading(false);
+            setNodes(els.nodes);
+            setEdges(els.edges);
+          }
+        })
+        .catch(() => alert("something went wrong!!! Could not render graph"));
+    }, 1000);
+  }, []);
   return (
     <>
       <Head
@@ -27,7 +141,13 @@ export const TraceView = () => {
           justifyContent="space-between"
           flex={1}
         >
-          <TraceGraph />
+          <TraceGraph
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            isLoading={isLoading}
+          />
           <TraceTags />
         </Stack>
 
