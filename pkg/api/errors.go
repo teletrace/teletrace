d4b377f3ebc20c5f337f201a8b20b4356649a406
Tav <therzlic@cisco.com>
2022-11-08 22:32:22 +0200

    removed unnecessary comments

--- pkg/api/errors.go
+++ pkg/api/errors.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Maps an error type to it's approprate http status
 var errorTypeToHttpStatus map[errorType]int = map[errorType]int{
 	inputError:  http.StatusBadRequest,
 	serverError: http.StatusInternalServerError,
@@ -14,19 +13,15 @@ var errorTypeToHttpStatus map[errorType]int = map[errorType]int{
 
 type errorType int
 
-// An enum of explicit error types
 const (
 	inputError  errorType = iota
 	serverError errorType = iota
 )
 
-// Represents an error http response
 type errorResponse struct {
 	Message string `json:"message"`
 }
 
-// Determine which errorResponse to build given a raised error
-// Respond with the appropriate http status and body
 func respondWithError(err error, errorType errorType, c *gin.Context) {
 	errorMessage := err.Error()
 	errResponse := &errorResponse{Message: errorMessage}
