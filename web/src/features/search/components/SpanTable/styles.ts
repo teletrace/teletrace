d52a22a8f97d54b11cec5ade72c2cc5dc5c59e98
Nir Feinblat <92082083+nirf-cisco@users.noreply.github.com>
2022-11-09 14:03:46 +0200

    fix: sticky table + filters scrollbar (#283)
    **What this PR does**:
    Table headers are now sticky on scroll
    Filters' bar is scrollable on small screens
    **Which issue(s) this PR fixes**:
    Fixes #101 
    
    **Checklist**
    
    - [ ] Tests updated
    - [ ] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

--- web/src/features/search/components/SpanTable/styles.ts
+++ web/src/features/search/components/SpanTable/styles.ts
@@ -1,12 +1,10 @@
 const styles = {
-  container: {
-    height: "100%",
-  },
   header: {
     "& tr:nth-of-type(odd)": {
       backgroundColor: "rgba(43, 45, 50, 1)",
     },
   },
+  paper: { display: "flex", minHeight: 0 },
 };
 
 export default styles;
