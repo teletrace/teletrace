62c1766cb1a4ecc69f2a2bda59e349181ae60573
Reut Baruch <reutbar@cisco.com>
2022-11-17 16:14:02 +0200

    fix padding

--- web/src/features/trace/components/TraceTags/ServiceSpanTagsAttributesList/styles.ts
+++ web/src/features/trace/components/TraceTags/ServiceSpanTagsAttributesList/styles.ts
@@ -3,24 +3,19 @@ export const styles = {
     fontSize: "25px",
   },
   accordion: {
-    // width: "100%",
-    padding: "0px 40px",
-    // margin: "0",
-    // borderColor: "red",
+    padding: "0px 52px",
   },
   accodionSummary: {
     flexDirection: "row-reverse",
     height: "60px",
-    // margin: "0",
-    // borderTopLeftRadius: "8px",
-    // borderTopRightRadius: "8px",
     backgroundColor: "#1B1C21",
+    padding: "0px",
     "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
       transform: "rotate(90deg)",
     },
   },
   typography: {
-    marginLeft: "15px",
+    marginLeft: "5px",
     fontWeight: "700",
   },
 };
