a7a01b39a74b48352a981b57e7e9a59aab3f1e84
Shaqued <shaquedg@cisco.com>
2022-11-20 14:32:38 +0200

    fix: add changes to global app theme (#434)
    **What this PR does**:
    
    **Which issue(s) this PR fixes**:
    Fixes #<issue number>
    
    **Checklist**
    
    - [ ] Tests updated
    - [ ] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

--- web/src/components/Elements/BasicElements/IconButton.stories.tsx
+++ web/src/components/Elements/BasicElements/IconButton.stories.tsx
@@ -1,6 +1,8 @@
 import { Box, IconButton } from "@mui/material";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
+import { theme } from "@/styles";
+
 import { PhotoCamera } from "../ResourceIcon";
 
 export default {
@@ -10,7 +12,7 @@ export default {
 
 const Template: ComponentStory<typeof IconButton> = (args) => (
   <Box>
-    <IconButton color="primary" aria-label="upload picture" {...args}>
+    <IconButton aria-label="upload picture" {...args}>
       <PhotoCamera />
     </IconButton>
   </Box>
@@ -18,6 +20,7 @@ const Template: ComponentStory<typeof IconButton> = (args) => (
 
 export const Primary = Template.bind({});
 export const Secondary = Template.bind({});
+export const FromProps = Template.bind({});
 
 Primary.args = {
   color: "primary",
@@ -25,4 +28,10 @@ Primary.args = {
 
 Secondary.args = {
   color: "secondary",
+};
+
+FromProps.args = {
+  sx: {
+    color: theme.palette.grey[200],
+  },
 };
