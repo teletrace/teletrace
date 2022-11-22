64fe169656e83a6d4be4a45ccc454f67b96ce4b5
shaqued <shaqued201@gmail.com>
2022-11-22 17:34:48 +0200

    lint

--- web/src/components/Elements/BasicElements/Typography.stories.tsx
+++ web/src/components/Elements/BasicElements/Typography.stories.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
