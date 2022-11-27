45955f963a8f0406449c6524efeb3a18a7714aa0
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-11-27 08:57:08 +0200

    feat(trace-view): display span details by selected graph node (#528)
    **What this PR does**:
    Renders span list based on selected graph node.
    
    **Which issue(s) this PR fixes**:
    Fixes #335 
    
    ![span-details-by-graph-node-1](https://user-images.githubusercontent.com/37577482/204098468-4b248dc7-86b4-4c1a-bf99-c9835a3b4c95.png)
    ![span-details-by-graph-node-2](https://user-images.githubusercontent.com/37577482/204098470-e559bd5d-8d00-4080-be48-fbf51f8619b7.png)

--- web/src/features/trace/routes/TraceView/TraceView.tsx
+++ web/src/features/trace/routes/TraceView/TraceView.tsx
@@ -10,6 +10,7 @@ import { useTraceQuery } from "../../api/traceQuery";
 import { SpanDetailsList } from "../../components/SpanDetailsList";
 import { TRACE_MOCK } from "../../components/trace-mock";
 import { TraceGraph } from "../../components/TraceGraph";
+import { GraphNode } from "../../components/TraceGraph/Graph/types";
 import { TraceTimeline } from "../../components/TraceTimeline";
 import { trace_res } from "../../types/TraceViewMock";
 import { styles } from "./styles";
@@ -30,8 +31,7 @@ export const TraceView = () => {
 const ProductionTraceView = () => {
   const { traceId } = useParams() as TraceViewUrlParams;
   const { isLoading, data: trace, error } = useTraceQuery(traceId);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [selectedNode, setSelectedNode] = useState({});
+  const [selectedNode, setSelectedNode] = useState<GraphNode | null>(null);
 
   if (isLoading) {
     return (
@@ -67,7 +67,7 @@ const ProductionTraceView = () => {
           flex={1}
         >
           <TraceGraph setSelectedNode={setSelectedNode} spans={trace} />
-          <SpanDetailsList spans={TRACE_MOCK} />
+          <SpanDetailsList spans={selectedNode?.spans} />
         </Stack>
         <Stack divider={<Divider orientation="vertical" flexItem />} flex={1}>
           <TraceTimeline trace={trace} />
@@ -79,7 +79,7 @@ const ProductionTraceView = () => {
 
 const MockDataTraceView = () => {
   const [trace, setTrace] = useState<InternalSpan[] | null>(null);
-  const [selectedNode, setSelectedNode] = useState({});
+  const [selectedNode, setSelectedNode] = useState<GraphNode | null>(null);
   const [spans, setSpans] = useState<InternalSpan[]>([]);
 
   useEffect(() => {
