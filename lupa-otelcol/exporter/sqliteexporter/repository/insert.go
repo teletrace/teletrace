fa896597d04c256358e48092b09d48f605d61049
Tav <therzlic@cisco.com>
2022-11-27 13:28:53 +0200

    Refactored insertion

--- lupa-otelcol/exporter/sqliteexporter/repository/insert.go
+++ lupa-otelcol/exporter/sqliteexporter/repository/insert.go
@@ -19,6 +19,32 @@ const (
 	Link                   = "link"
 )
 
+func performInsert(tx *sql.Tx, query string, data ...any) (*int64, error) {
+	insertStmt, err := tx.Prepare(query)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not prepare statement: %+v\n", err)
+	}
+
+	insertResult, err := insertStmt.Exec(data...)
+	if err != nil {
+		return nil, fmt.Errorf("could not execute statement: %v\n", err)
+	}
+
+	linkId, err := insertResult.LastInsertId()
+
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve auto-generated id: %v\n", err)
+	}
+
+	err = insertStmt.Close()
+	if err != nil {
+		return nil, fmt.Errorf("could not close statement: %v\n", err)
+	}
+
+	return &linkId, nil
+}
+
 func InsertAttribute(tx *sql.Tx, attributeKind AttributeKind, id any, key string, value any, valueType string) error {
 	var tableName string
 	var idName string
@@ -40,89 +66,22 @@ func InsertAttribute(tx *sql.Tx, attributeKind AttributeKind, id any, key string
 		idName = "link_id"
 	}
 
-	insertSpanAttributeStmt, err := tx.Prepare(
+	_, err := performInsert(tx,
 		fmt.Sprintf("INSERT INTO %s (%s, key, value, type) VALUES (?, ?, ?, ?)", tableName, idName),
+		id, key, value, valueType,
 	)
 
-	if err != nil {
-		return fmt.Errorf("could not prepare statement: %+v\n", err)
-	}
-
-	if _, err := insertSpanAttributeStmt.Exec(id, key, value, valueType); err != nil {
-		return fmt.Errorf("could not execute statement: %v\n", err)
-	}
-
-	err = insertSpanAttributeStmt.Close()
-	if err != nil {
-		return fmt.Errorf("could not close statement: %v\n", err)
-	}
-
-	return nil
-}
-
-func InsertLink(tx *sql.Tx, link ptrace.SpanLink, spanId string) (*int64, error) {
-	insertLinkStmt, err := tx.Prepare(
-		"INSERT INTO links (span_id, trace_state, dropped_attributes_count) VALUES (?, ?, ?)",
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("could not prepare statement: %+v\n", err)
-	}
-
-	insertLinkResult, err := insertLinkStmt.Exec(spanId, link.TraceState().AsRaw(), link.DroppedAttributesCount())
-	if err != nil {
-		return nil, fmt.Errorf("could not execute statement: %v\n", err)
-	}
-
-	linkId, err := insertLinkResult.LastInsertId()
-
-	if err != nil {
-		return nil, fmt.Errorf("could not retrieve auto-generated event id: %v\n", err)
-	}
-
-	err = insertLinkStmt.Close()
-	if err != nil {
-		return nil, fmt.Errorf("could not close statement: %v\n", err)
-	}
-
-	return &linkId, nil
-}
-
-func InsertEvent(tx *sql.Tx, event ptrace.SpanEvent, spanId string) (*int64, error) {
-	insertEventStmt, err := tx.Prepare(
-		"INSERT INTO events (span_id, name, time_unix_nano, dropped_attributes_count) VALUES (?, ?, ?, ?)",
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("could not prepare statement: %+v\n", err)
-	}
-
-	eventResult, err := insertEventStmt.Exec(
-		spanId, event.Name(), uint64(event.Timestamp()), event.DroppedAttributesCount())
-
-	if err != nil {
-		return nil, fmt.Errorf("could not execute statement: %v\n", err)
-	}
-
-	eventId, err := eventResult.LastInsertId()
-
-	if err != nil {
-		return nil, fmt.Errorf("could not retrieve auto-generated event id: %v\n", err)
-	}
-
-	err = insertEventStmt.Close()
-	if err != nil {
-		return nil, fmt.Errorf("could not close statement: %v\n", err)
-	}
-
-	return &eventId, nil
+	return err
 }
 
 func InsertSpan(
 	tx *sql.Tx, span ptrace.Span, spanId string, droppedResourceAttributesCount uint32, resourceId uuid.UUID,
 	scopeId int64,
 ) error {
-	insertSpanStmt, err := tx.Prepare(`
+	duration := span.EndTimestamp() - span.StartTimestamp()
+	ingestionTimeUnixNano := uint64(time.Now().UTC().Nanosecond()) //  TODO: Check if this is a duration or a timestamp
+
+	_, err := performInsert(tx, `
 		INSERT INTO spans (
 			span_id, trace_id, trace_state, 
 		    parent_span_id, name, kind, start_time_unix_nano, 
@@ -130,56 +89,35 @@ func InsertSpan(
 		    span_status_message, span_status_code, dropped_resource_attributes_count, duration, 
 		    ingestion_time_unix_nano, instrumentation_scope_id, resource_id
 		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,	?, ?, ?, ?)
-	`)
-
-	if err != nil {
-		return fmt.Errorf("could not prepare statement: %+v\n", err)
-	}
-
-	duration := span.EndTimestamp() - span.StartTimestamp()
-	ingestionTimeUnixNano := uint64(time.Now().UTC().Nanosecond()) //  TODO: Check if this is a duration or a timestamp
-	if _, err := insertSpanStmt.Exec(
+	`,
 		spanId, span.TraceID().HexString(), span.TraceState().AsRaw(),
 		span.ParentSpanID().HexString(), span.Name(), int32(span.Kind()), uint64(span.StartTimestamp()),
 		uint64(span.EndTimestamp()), span.DroppedAttributesCount(), span.DroppedEventsCount(), span.DroppedLinksCount(),
 		span.Status().Message(), span.Status().Code(), droppedResourceAttributesCount, duration,
 		ingestionTimeUnixNano, scopeId, resourceId,
-	); err != nil {
-		return fmt.Errorf("could not execute statement: %v\n", err)
-	}
+	)
 
-	err = insertSpanStmt.Close()
-	if err != nil {
-		return fmt.Errorf("could not close statement: %v\n", err)
-	}
+	return err
+}
 
-	return nil
+func InsertLink(tx *sql.Tx, link ptrace.SpanLink, spanId string) (*int64, error) {
+	return performInsert(tx,
+		"INSERT INTO links (span_id, trace_state, dropped_attributes_count) VALUES (?, ?, ?)",
+		spanId, link.TraceState().AsRaw(), link.DroppedAttributesCount(),
+	)
+}
+
+func InsertEvent(tx *sql.Tx, event ptrace.SpanEvent, spanId string) (*int64, error) {
+	return performInsert(
+		tx,
+		"INSERT INTO events (span_id, name, time_unix_nano, dropped_attributes_count) VALUES (?, ?, ?, ?)",
+		spanId, event.Name(), uint64(event.Timestamp()), event.DroppedAttributesCount(),
+	)
 }
 
 func InsertScope(tx *sql.Tx, scope pcommon.InstrumentationScope) (*int64, error) {
-	insertScopeStmt, err := tx.Prepare(
+	return performInsert(tx,
 		"INSERT INTO scopes (name, version, dropped_attributes_count) VALUES (?, ?, ?)",
+		scope.Name(), scope.Version(), scope.DroppedAttributesCount(),
 	)
-
-	if err != nil {
-		return nil, fmt.Errorf("could not prepare statement: %+v\n", err)
-	}
-
-	scopeResult, err := insertScopeStmt.Exec(scope.Name(), scope.Version(), scope.DroppedAttributesCount())
-	if err != nil {
-		return nil, fmt.Errorf("could not execute statement: %v\n", err)
-	}
-
-	scopeId, err := scopeResult.LastInsertId()
-
-	if err != nil {
-		return nil, fmt.Errorf("could not retrieve auto-generated scope id: %v\n", err)
-	}
-
-	err = insertScopeStmt.Close()
-	if err != nil {
-		return nil, fmt.Errorf("could not close statement: %v\n", err)
-	}
-
-	return &scopeId, nil
 }
