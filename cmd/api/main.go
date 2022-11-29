f89955d476c6f2974b93f30d6e9f3715f310e334
Timur Misharin <tmishari@cisco.com>
2022-11-29 15:28:47 +0200

    changes connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,12 +18,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"oss-tracing/pkg/api"
 	"oss-tracing/pkg/config"
 	"oss-tracing/pkg/logs"
 	spanreaderes "oss-tracing/plugin/spanreader/es"
+	sqlite "oss-tracing/plugin/spanreader/sqlite"
 
 	"go.uber.org/zap"
 )
@@ -39,6 +41,10 @@ func main() {
 		log.Fatalf("Failed to initialize logger: %v", err)
 	}
 	defer logs.FlushBufferedLogs(logger)
+
+	sr2, err := sqlite.NewSqliteSpanReader(logger, &sqlite.Config{})
+
+	fmt.Println(sr2)
 
 	sr, err := spanreaderes.NewSpanReader(context.Background(), logger, spanreaderes.NewElasticConfig(cfg))
 	if err != nil {
