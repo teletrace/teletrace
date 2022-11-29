5de22b327e107642e1463d91c0705a185aa9a52b
Ophir Yael <37577482+ophiryael@users.noreply.github.com>
2022-11-29 13:19:29 +0200

    Support selectedSpanId in TraceTimeline

--- web/src/features/trace/components/TraceTimeline/TraceTimelineViewer/VirtualizedTraceView/SpanBarRow.js
+++ web/src/features/trace/components/TraceTimeline/TraceTimelineViewer/VirtualizedTraceView/SpanBarRow.js
@@ -42,8 +42,7 @@ export default class SpanBarRow extends React.PureComponent {
       hoverIndentGuideIds,
       removeHoverIndentGuideId,
       addHoverIndentGuideId,
-      setActiveTimelineState,
-      activeSpan,
+      selectedSpanId,
     } = this.props;
     const { duration, hasChildren: isParent, operationName, process } = span;
     const serviceName = process.serviceName;
@@ -78,9 +77,8 @@ export default class SpanBarRow extends React.PureComponent {
           span-row
           ${className || ""}
           ${isDetailExpanded ? "is-expanded" : ""}
-          ${activeSpan === span.spanID ? "is-active" : ""}
+          ${selectedSpanId === span.spanID ? "is-active" : ""}
         `}
-        onClick={() => setActiveTimelineState(span)}
       >
         <TimelineRow.Cell className="span-name-column" width={columnDivision}>
           <div className="span-name-wrapper">
