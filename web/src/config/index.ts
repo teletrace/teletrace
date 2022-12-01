14d060d4c8a2bfe9a6ff3471f785b49a48e20b40
tvaintrob <vintrob@cisco.com>
2022-12-01 15:55:57 +0200

    updated makefile and added license header to all files

--- web/src/config/index.ts
+++ web/src/config/index.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Epsagon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // this file contains configuration for the application such as API_URL, etc.
 const LOCAL_API_URL = "http://localhost:8080";
 export const API_URL = process.env.REACT_APP_API_URL ?? LOCAL_API_URL;
