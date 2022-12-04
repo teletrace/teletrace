9e3855a614311bc3db65c5eaa38b3894cac10807
Tav <therzlic@cisco.com>
2022-12-04 16:42:14 +0200

    pre-commit

--- web/src/features/search/components/SpanTable/SpanTable.tsx
+++ web/src/features/search/components/SpanTable/SpanTable.tsx
@@ -21,7 +21,7 @@ import MaterialReactTable, {
   Virtualizer,
 } from "material-react-table";
 import { useEffect, useRef, useState } from "react";
-import {useDebouncedCallback} from "use-debounce";
+import { useDebouncedCallback } from "use-debounce";
 
 import {
   formatDateAsDateTime,
@@ -80,14 +80,13 @@ export function SpanTable({ filters = [], timeframe }: SpanTableProps) {
       )
     ) ?? [];
 
-    const debouncedFetchNextPage = useDebouncedCallback(fetchNextPage, 100)
-    const fetchMoreOnBottomReached = (tableWrapper: HTMLDivElement) => {
+  const debouncedFetchNextPage = useDebouncedCallback(fetchNextPage, 100);
+  const fetchMoreOnBottomReached = (tableWrapper: HTMLDivElement) => {
     const { scrollHeight, scrollTop, clientHeight } = tableWrapper;
     if (scrollHeight - scrollTop - clientHeight < 200 && !isFetching) {
-        debouncedFetchNextPage();
+      debouncedFetchNextPage();
     }
   };
-
 
   const tableWrapper = tableWrapperRef.current;
   useEffect(() => {
