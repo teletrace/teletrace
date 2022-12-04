6176cda3002d8b30bfc6e9b82549f7ff0c1cf252
Tav <therzlic@cisco.com>
2022-12-04 13:47:26 +0200

    reverted batch size to 200

--- plugin/spanreader/es/searchcontroller/search_controller.go
+++ plugin/spanreader/es/searchcontroller/search_controller.go
@@ -97,7 +97,7 @@ func buildSearchRequest(r spansquery.SearchRequest) (*search.Request, error) {
 		builder = builder.SearchAfter(sortResultsBuilder)
 	}
 
-	builder = builder.Size(3) // Where to get this number from?
+	builder = builder.Size(200) // Where to get this number from?
 
 	return builder.Build(), nil
 }
