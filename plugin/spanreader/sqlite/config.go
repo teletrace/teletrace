a81265f42ecae0037c6295cc54893e0dfc3147a1
Timur Misharin <tmishari@cisco.com>
2022-12-04 14:42:40 +0200

    feat: sqlite connection for query

--- plugin/spanreader/sqlite/config.go
+++ plugin/spanreader/sqlite/config.go
@@ -1,20 +1,13 @@
 package sqlite
 
-import (
-	"go.opentelemetry.io/collector/config"
-)
-
-type Config struct {
-	config.ExporterSettings `mapstructure:",squash"`
-	DBSettings              SqliteConfig `mapstructure:"db"`
-}
+import "oss-tracing/pkg/config"
 
 type SqliteConfig struct {
 	Path string
 }
 
-func NewSqliteConfig(path string) *SqliteConfig {
-	return &SqliteConfig{
-		Path: path,
+func NewSqliteConfig(cfg config.Config) SqliteConfig {
+	return SqliteConfig{
+		Path: cfg.SQLitePath,
 	}
 }
