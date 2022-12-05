df9e930b2a5a95cd7ae62d48e2a4f87d273927fa
Peleg Naohr <pelegnahor@gmail.com>
2022-12-05 09:49:06 +0200

    add time-frame-selector feat

---
+++ web/src/features/search/components/DateTimeSelector/DateTimeSelector.tsx
@@ -0,0 +1,113 @@
+import { Alert, Button, DialogContent, Stack, TextField } from "@mui/material";
+import { DatePicker, TimePicker } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { useState } from "react";
+import { Timeframe } from "../../types/common";
+
+export type DateTimeSelectorProps = {
+  onChange: (timeframe: Timeframe) => void;
+  onClose: () => void;
+};
+
+export const DateTimeSelector = ({
+  onChange,
+  onClose,
+}: DateTimeSelectorProps) => {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+  const [startTime, setStartTime] = useState<Date | null>(
+    new Date(new Date().setHours(new Date().getHours() - 1))
+  );
+  const [endTime, setEndTime] = useState<Date | null>(new Date());
+  const [timeValid, setTimeValid] = useState<boolean>(true);
+  const [timeframe, setTimeframe] = useState<Timeframe>({
+    startTimeUnixNanoSec: new Date().setHours(new Date().getHours() - 1),
+    endTimeUnixNanoSec: new Date().getTime(),
+  });
+
+  const calcRange = () => {
+    const startRange = new Date(
+      startDate!.setHours(startTime!.getHours(), startTime!.getMinutes())
+    ).getTime();
+    const endRange = new Date(
+      endDate!.setHours(endTime!.getHours(), endTime!.getMinutes())
+    ).getTime();
+    setTimeframe({
+      startTimeUnixNanoSec: startRange,
+      endTimeUnixNanoSec: endRange,
+    });
+
+    return { startTimeUnixNanoSec: startRange, endTimeUnixNanoSec: endRange };
+  };
+
+  const handleApply = () => {
+    const timeRange = calcRange();
+    const isTimeValid =
+      timeRange.startTimeUnixNanoSec < timeRange.endTimeUnixNanoSec;
+    setTimeValid(isTimeValid);
+    if (isTimeValid) {
+      onChange(timeRange);
+      onClose();
+    }
+  };
+
+  const handleCancle = () => {
+    setStartDate(new Date());
+    setEndDate(new Date());
+    setStartTime(new Date(new Date().setHours(new Date().getHours() - 1)));
+    setEndTime(new Date());
+
+    onClose();
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DialogContent>
+        From
+        <Stack direction="row">
+          <DatePicker
+            inputFormat="dd-MM-yyyy"
+            onChange={(startDate) => setStartDate(startDate)}
+            renderInput={(props) => <TextField {...props} />}
+            value={startDate}
+          />
+          <TimePicker
+            ampm={false}
+            onChange={(startTime) => setStartTime(startTime)}
+            value={startTime}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+        To
+        <Stack direction="row">
+          <DatePicker
+            inputFormat="dd-MM-yyyy"
+            renderInput={(props) => <TextField {...props} />}
+            onChange={(endDate) => {
+              setEndDate(endDate);
+            }}
+            value={endDate}
+          />
+          <TimePicker
+            ampm={false}
+            onChange={(endTime) => {
+              setEndTime(endTime);
+            }}
+            value={endTime}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </DialogContent>
+      {!timeValid ? (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert severity="error">Please enter a valid date range</Alert>
+        </Stack>
+      ) : null}
+      <Stack direction="row" justifyContent="flex-end">
+        <Button onClick={handleCancle}>Cancel</Button>
+        <Button onClick={handleApply}>Apply</Button>
+      </Stack>
+    </LocalizationProvider>
+  );
+};
---
+++ web/src/features/search/components/TimeFrameSelector/TimeFrameSelector.tsx
@@ -0,0 +1,171 @@
+import {
+  DialogContent,
+  Popover,
+  ToggleButton,
+  ToggleButtonGroup,
+  Tooltip,
+} from "@mui/material";
+import { useRef, useState, MouseEvent } from "react";
+import { Timeframe } from "../../types/common";
+import { DateTimeSelector } from "../DateTimeSelector/DateTimeSelector";
+import { formatDateToTimeString } from "@/utils/format";
+
+export type TimeFrameSelectorProps = {
+  onChange: (timeframe: Timeframe) => void;
+};
+
+const options: RelativeTimeFrame[] = [
+  { label: "1H", offsetRange: "1h", relativeTo: "now" },
+  { label: "1D", offsetRange: "1d", relativeTo: "now" },
+  { label: "3D", offsetRange: "3d", relativeTo: "now" },
+  { label: "1W", offsetRange: "1w", relativeTo: "now" },
+];
+
+export const TimeFrameSelector = ({ onChange }: TimeFrameSelectorProps) => {
+  const customOption: CustomTimeFrame = {
+    label: "Custom",
+    startTime: new Date(),
+    endTime: new Date(),
+  };
+
+  const buttonRef = useRef(null);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [open, setOpen] = useState(false);
+  const [isSelected, setIsSelected] = useState<TimeFrameTypes>(options[0]);
+
+  const [timeframe, setTimeFrame] = useState<Timeframe>();
+
+  const handleCustomClick = (event: MouseEvent<HTMLElement>) => {
+    setOpen(true);
+    setAnchorEl(event.currentTarget);
+  };
+
+  function isRelativeTimeFrame(object: any): object is RelativeTimeFrame {
+    return "offsetRange" in object;
+  }
+
+  function isCustomTimeFrame(object: any): object is CustomTimeFrame {
+    return "startTime" in object;
+  }
+
+  const toTimeframeFromRelative = (timeFrame: RelativeTimeFrame) => {
+    const startTime = new Date();
+    const endTimeNumber = new Date().getTime();
+    const offset = timeFrame.offsetRange;
+    if (offset === "1h") startTime.setHours(startTime.getHours() - 1);
+    else if (offset === "1d") startTime.setDate(startTime.getDate() - 1);
+    else if (offset === "3d") startTime.setDate(startTime.getDate() - 3);
+    else if (offset === "1w") startTime.setDate(startTime.getDate() - 7);
+    const startTimeNumber = startTime.getTime();
+    return {
+      startTimeUnixNanoSec: startTimeNumber,
+      endTimeUnixNanoSec: endTimeNumber,
+    };
+  };
+
+  const toTimeframeFromCustom = (timeFrame: CustomTimeFrame) => {
+    return {
+      startTimeUnixNanoSec: timeFrame.startTime.getTime(),
+      endTimeUnixNanoSec: timeFrame.endTime.getTime(),
+    };
+  };
+
+  const calcTimeFrame = (timeFrame: TimeFrameTypes) => {
+    if (isRelativeTimeFrame(timeFrame)) {
+      return toTimeframeFromRelative(timeFrame);
+    } else if (isCustomTimeFrame(timeFrame)) {
+      return toTimeframeFromCustom(timeFrame);
+    }
+  };
+
+  const handleBtnClicked = (
+    event: MouseEvent<HTMLElement>,
+    value: TimeFrameTypes
+  ) => {
+    if (value?.label === "Custom") {
+      handleCustomClick(event);
+    }
+    const timeFrame = calcTimeFrame(value);
+    setTimeFrame(timeFrame);
+    onChange(timeFrame!);
+    setIsSelected(value);
+  };
+
+  const getTooltipTitle = (): string =>
+    `${formatDateToTimeString(
+      timeframe?.startTimeUnixNanoSec || 0
+    )} -> ${formatDateToTimeString(timeframe?.endTimeUnixNanoSec || 0)}`;
+
+  return (
+    <div>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        onClose={() => setOpen(false)}
+      >
+        <DialogContent>
+          <DateTimeSelector
+            onChange={(timeframe) => {
+              setTimeFrame(timeframe);
+              onChange(timeframe);
+            }}
+            onClose={() => setOpen(false)}
+          />
+        </DialogContent>
+      </Popover>
+      <ToggleButtonGroup exclusive>
+        <ToggleButton
+          onClick={handleBtnClicked}
+          selected={isSelected?.label === customOption?.label}
+          value={customOption}
+          ref={buttonRef}
+          key={customOption.label}
+        >
+          {isSelected?.label === customOption?.label
+            ? getTooltipTitle()
+            : customOption.label}
+        </ToggleButton>
+
+        {options.map((tf, idx) => (
+          <Tooltip
+            title={isSelected?.label === tf?.label ? getTooltipTitle() : ""}
+            placement="top-end"
+            arrow
+          >
+            <ToggleButton
+              onClick={handleBtnClicked}
+              selected={isSelected?.label === tf?.label}
+              value={tf}
+              ref={buttonRef}
+              key={tf.label}
+            >
+              {tf.label}
+            </ToggleButton>
+          </Tooltip>
+        ))}
+      </ToggleButtonGroup>
+    </div>
+  );
+};
+
+type RelativeTimeFrame = {
+  label: string;
+  relativeTo: string;
+  offsetRange: string;
+};
+
+type CustomTimeFrame = {
+  label: string;
+  startTime: Date;
+  endTime: Date;
+};
+
+export type TimeFrameTypes = RelativeTimeFrame | CustomTimeFrame;
