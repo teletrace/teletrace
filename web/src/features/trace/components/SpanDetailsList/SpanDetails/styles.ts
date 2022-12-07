988db8ca56a99a4ecc449d70797267505e109434
Reut Baruch <89512916+BaruchReut@users.noreply.github.com>
2022-12-07 23:30:57 +0200

    feat(trace-view): add error icon next to span with errors (#722)
    **What this PR does**:
    Adds error icon next to a specific span with errors in span details list view.
    
    **Which issue(s) this PR fixes**:
    Fixes #709 
    
    **Additional Information**
    After discussing the design with @adva-cisco it was decided to make the left margin of the span list smaller than the original design in [Figma](https://www.figma.com/file/XHENquLcsodYHHKb923uM1/Telescope?node-id=190%3A6458&t=wssguRhSQt3tivCk-0).

--- web/src/features/trace/components/SpanDetailsList/SpanDetails/styles.ts
+++ web/src/features/trace/components/SpanDetailsList/SpanDetails/styles.ts
@@ -16,8 +16,8 @@
 
 export const styles = {
   accordion: {
+    width: "100%",
     borderRadius: "8px",
-    marginBottom: "8px",
   },
   accordionSummary: {
     flexDirection: "row-reverse",
@@ -36,6 +36,31 @@ export const styles = {
   expandArrowIcon: {
     color: "#FFFFFF",
     fontSize: "15px",
+  },
+  errorIcon: {
+    width: "20px",
+    height: "20px",
+    color: "#EF5854",
+  },
+  spanMainContainer: {
+    display: "flex",
+    marginLeft: "36px",
+    marginBottom: "8px",
+  },
+  spanErrorMainContainer: {
+    marginLeft: "0px",
+  },
+  spanErrorIconContainer: {
+    marginRight: "16px",
+    marginTop: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  expandedSpanErrorContainer: {
+    backgroundColor: "#3B3C42",
+    flex: "1",
+    width: "1px",
   },
   spanFlowIconsContainer: {
     flexDirection: "row",
