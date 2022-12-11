c5f42d36dfee0a670c04affb1b12f2f622898016
Tav <therzlic@cisco.com>
2022-12-11 10:05:46 +0200

    precommit

--- web/src/features/search/components/SpanTable/columns.tsx
+++ web/src/features/search/components/SpanTable/columns.tsx
@@ -47,8 +47,10 @@ export const columns: ColumnDef<TableSpan>[] = [
     enableSorting: true,
     Cell: (mrtCell) => {
       const durationMillis = mrtCell.cell.getValue() as number;
-      const durationString = durationMillis < 0.01 ? 
-      LOW_PRECISION_DURATION_DISPLAY : durationMillis.toString()
+      const durationString =
+        durationMillis < 0.01
+          ? LOW_PRECISION_DURATION_DISPLAY
+          : durationMillis.toString();
       return `${durationString}ms`;
     },
   },
