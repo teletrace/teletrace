a94286659d8ea4112cb92fa9eae7bae4c5b99e72
Itay Ozery <iozery@cisco.com>
2022-12-15 15:46:34 +0200

    fix: remove Set redundent definition

--- web/src/features/trace/utils/span-resource-type.ts
+++ web/src/features/trace/utils/span-resource-type.ts
@@ -21,10 +21,10 @@ export const RESOURCE_TYPES = ["db.system", "db.type", "messaging.system"];
 export function getSpanResourceType(span: Readonly<InternalSpan>): string {
   const spanAttr = span.span.attributes;
 
-  const resourceTypeSet = new Set<string>(RESOURCE_TYPES);
-
-  for (const key of resourceTypeSet) {
-    if (spanAttr[key]) return spanAttr[key].toString();
+  for (const key of RESOURCE_TYPES) {
+    if (spanAttr[key]) {
+      return spanAttr[key].toString();
+    }
   }
 
   return "";
