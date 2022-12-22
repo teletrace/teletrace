a8a434a2f2eb57f4803cd5984a42b4c96c765053
Itay Ozery <iozery@cisco.com>
2022-12-22 13:05:59 +0200

    adding demo client-server code

---
+++ deploy/demo/client/main.go
@@ -0,0 +1,150 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Sample contains a simple client that periodically makes a simple http request
+// to a server and exports to the OpenTelemetry service.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"google.golang.org/grpc"
+)
+
+// Initializes an OTLP exporter, and configures the corresponding trace and
+// metric providers.
+func initProvider() func() {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithProcess(),
+		resource.WithTelemetrySDK(),
+		resource.WithHost(),
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceNameKey.String("demo-client"),
+		),
+	)
+	handleErr(err, "failed to create resource")
+
+	otelAgentAddr, ok := os.LookupEnv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	if !ok {
+		otelAgentAddr = "0.0.0.0:4317"
+	}
+
+	traceClient := otlptracegrpc.NewClient(
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(otelAgentAddr),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()))
+	sctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	traceExp, err := otlptrace.New(sctx, traceClient)
+	handleErr(err, "Failed to create the collector trace exporter")
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExp)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+
+	// set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	otel.SetTracerProvider(tracerProvider)
+
+	return func() {
+		cxt, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+		if err := traceExp.Shutdown(cxt); err != nil {
+			otel.Handle(err)
+		}
+	}
+}
+
+func handleErr(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s: %v", message, err)
+	}
+}
+
+func main() {
+	shutdown := initProvider()
+	defer shutdown()
+
+	tracer := otel.Tracer("demo-client-tracer")
+
+	method, _ := baggage.NewMember("method", "repl")
+	client, _ := baggage.NewMember("client", "cli")
+	bag, _ := baggage.New(method, client)
+
+	defaultCtx := baggage.ContextWithBaggage(context.Background(), bag)
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for {
+		startTime := time.Now()
+		ctx, span := tracer.Start(defaultCtx, "ExecuteRequest")
+		makeRequest(ctx)
+		span.End()
+		latencyMs := float64(time.Since(startTime)) / 1e6
+		nr := int(rng.Int31n(7))
+		for i := 0; i < nr; i++ {
+			randLineLength := rng.Int63n(999)
+			fmt.Printf("#%d: LineLength: %dBy\n", i, randLineLength)
+		}
+
+		fmt.Printf("Latency: %.3fms\n", latencyMs)
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+}
+
+func makeRequest(ctx context.Context) {
+
+	demoServerAddr, ok := os.LookupEnv("DEMO_SERVER_ENDPOINT")
+	if !ok {
+		demoServerAddr = "http://0.0.0.0:7080/hello"
+	}
+
+	// Trace an HTTP client by wrapping the transport
+	client := http.Client{
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
+	}
+
+	// Make sure we pass the context to the request to avoid broken traces.
+	req, err := http.NewRequestWithContext(ctx, "GET", demoServerAddr, nil)
+	if err != nil {
+		handleErr(err, "failed to http request")
+	}
+
+	// All requests made with this client will create spans.
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	res.Body.Close()
+}
