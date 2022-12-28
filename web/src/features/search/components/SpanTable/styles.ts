eb69e1cbad09bdd7e9f11e12d69411599efe1526
Nir Feinblat <92082083+nirf-cisco@users.noreply.github.com>
2022-12-28 11:23:14 +0200

    refactor: using virtualization in the spans table (#888)
    ## What this PR does:
    Front:
    * Bump material-react-table to 1.5.2
    * Add virtualization to the table
    * table state optimization: use memo for search request, remove redundant rerenders
    * Fix table loader (was disabled)  #877 
    Backend:
    * Reduce page size to 50
    ## Which issue(s) this PR fixes:
    
    Fixes #877 
    Fixes #886 
    
    ## Checklist
    
    - [ ] Tests updated
    - [ ] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

--- web/src/features/search/components/SpanTable/styles.ts
+++ web/src/features/search/components/SpanTable/styles.ts
@@ -29,7 +29,7 @@ const newRowAnimation = keyframes`
  `;
 
 const styles: { [name: string]: CSSProperties } = {
-  progress: { margin: 0, position: "absolute", top: 47, right: 0, left: 0 },
+  progress: { margin: 0, position: "absolute", top: 45, right: 0, left: 0 },
   container: { minHeight: 0, position: "relative", borderRadius: "8px" },
   tablePaper: { display: "flex", maxHeight: "100%" },
   tableContainer: { borderRadius: "8px" },
