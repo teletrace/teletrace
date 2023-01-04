6f91c92f378503b985929f0c2fceef3edba34352
Reut Baruch <reutbar@cisco.com>
2023-01-04 14:44:19 +0200

    CR notes

--- tests/e2e/playwright/playwright.config.ts
+++ tests/e2e/playwright/playwright.config.ts
@@ -27,7 +27,7 @@ import { devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: "./example-tests",
+  testDir: "./tests",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -38,13 +38,13 @@ const config: PlaywrightTestConfig = {
     timeout: 5000,
   },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- tests/e2e/playwright/tests/example.spec.ts
+++ tests/e2e/playwright/tests/example.spec.ts
@@ -17,7 +17,7 @@
 import { test, expect } from "@playwright/test";
 
 test("homepage has title", async ({ page }) => {
-  await page.goto("https://app.lupaproject.io/");
+  await page.goto("https://localhost:8080");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Lupa/);
