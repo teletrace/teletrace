718f1d34f221a91a0a6dbf06f32823d9112365a4
Reut Baruch <reutbar@cisco.com>
2023-01-04 14:49:05 +0200

    run test on localhost

--- tests/e2e/playwright/tests/example.spec.ts
+++ tests/e2e/playwright/tests/example.spec.ts
@@ -17,7 +17,7 @@
 import { test, expect } from "@playwright/test";
 
 test("homepage has title", async ({ page }) => {
-  await page.goto("https://localhost:8080");
+  await page.goto("http://localhost:8080/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Lupa/);
