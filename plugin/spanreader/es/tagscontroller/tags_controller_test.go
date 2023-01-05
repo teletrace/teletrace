e515d189dfd47b729851e4f7009aa576ec548646
Roy Ben Haroosh <roybh@cisco.com>
2023-01-05 16:40:18 +0200

    minor lint fix and unittest

--- plugin/spanreader/es/tagscontroller/tags_controller_test.go
+++ plugin/spanreader/es/tagscontroller/tags_controller_test.go
@@ -17,20 +17,20 @@
 package tagscontroller
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"oss-tracing/pkg/model"
-	"oss-tracing/pkg/model/tagsquery/v1"
-	"testing"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "oss-tracing/pkg/model"
+    "oss-tracing/pkg/model/tagsquery/v1"
+    "testing"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 )
 
 func Test_ParseGetTagsValuesResponseBody_ValidResponse(t *testing.T) {
-	// Arrange
-	// Elastic search response
-	responseContent := `
+    // Arrange
+    // Elastic search response
+    responseContent := `
 	{
 		"_shards": {
 		 "failed": 0,
@@ -99,108 +99,108 @@ func Test_ParseGetTagsValuesResponseBody_ValidResponse(t *testing.T) {
 	}
 	`
 
-	buffer := bytes.NewBufferString(responseContent)
-	body := make(map[string]any)
-	_ = json.NewDecoder(buffer).Decode(&body)
+    buffer := bytes.NewBufferString(responseContent)
+    body := make(map[string]any)
+    _ = json.NewDecoder(buffer).Decode(&body)
 
-	// Act
-	uut := tagsController{}
-	result, err := uut.parseGetTagsValuesResponseBody(body)
-	if err != nil {
-		t.Fatal(err)
-	}
+    // Act
+    uut := tagsController{}
+    result, err := uut.parseGetTagsValuesResponseBody(body)
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	// Assert
-	assert.Len(t, result, 3)
-	assert.Contains(t, result, "span.attributes.http.method.keyword")
-	assert.Contains(t, result, "span.attributes.http.flavor.keyword")
-	assert.Contains(t, result, "span.attributes.http.status_code")
+    // Assert
+    assert.Len(t, result, 3)
+    assert.Contains(t, result, "span.attributes.http.method.keyword")
+    assert.Contains(t, result, "span.attributes.http.flavor.keyword")
+    assert.Contains(t, result, "span.attributes.http.status_code")
 
-	assertTag := func(tag string, expectedInfos tagsquery.TagValuesResponse) {
-		assert.Contains(t, result, tag)
-		assert.ElementsMatch(t, result[tag].Values, expectedInfos.Values)
-	}
+    assertTag := func(tag string, expectedInfos tagsquery.TagValuesResponse) {
+        assert.Contains(t, result, tag)
+        assert.ElementsMatch(t, result[tag].Values, expectedInfos.Values)
+    }
 
-	assertTag(
-		"span.attributes.http.method.keyword",
-		tagsquery.TagValuesResponse{
-			Values: []tagsquery.TagValueInfo{{
-				Value: "GET",
-				Count: 4,
-			}, {
-				Value: "POST",
-				Count: 1,
-			}, {
-				Value: "DELETE",
-				Count: 1,
-			}},
-		},
-	)
+    assertTag(
+        "span.attributes.http.method.keyword",
+        tagsquery.TagValuesResponse{
+            Values: []tagsquery.TagValueInfo{{
+                Value: "GET",
+                Count: 4,
+            }, {
+                Value: "POST",
+                Count: 1,
+            }, {
+                Value: "DELETE",
+                Count: 1,
+            }},
+        },
+    )
 
-	assertTag(
-		"span.attributes.http.flavor.keyword",
-		tagsquery.TagValuesResponse{
-			Values: []tagsquery.TagValueInfo{
-				{
-					Value: "1.0",
-					Count: 6,
-				},
-			},
-		},
-	)
+    assertTag(
+        "span.attributes.http.flavor.keyword",
+        tagsquery.TagValuesResponse{
+            Values: []tagsquery.TagValueInfo{
+                {
+                    Value: "1.0",
+                    Count: 6,
+                },
+            },
+        },
+    )
 
-	assertTag(
-		"span.attributes.http.status_code",
-		tagsquery.TagValuesResponse{
-			Values: []tagsquery.TagValueInfo{
-				{
-					Value: float64(200),
-					Count: 3,
-				},
-				{
-					Value: float64(404),
-					Count: 2,
-				},
-				{
-					Value: float64(500),
-					Count: 1,
-				},
-			},
-		},
-	)
+    assertTag(
+        "span.attributes.http.status_code",
+        tagsquery.TagValuesResponse{
+            Values: []tagsquery.TagValueInfo{
+                {
+                    Value: float64(200),
+                    Count: 3,
+                },
+                {
+                    Value: float64(404),
+                    Count: 2,
+                },
+                {
+                    Value: float64(500),
+                    Count: 1,
+                },
+            },
+        },
+    )
 }
 
 func Test_RemoveDuplicatedTextTags_RemoveTextDuplicates(t *testing.T) {
-	tagsMock := []tagsquery.TagInfo{
-		{
-			Name: "span.attributes.http.method.keyword",
-			Type: "keyword",
-		},
-		{
-			Name: "span.attributes.http.method",
-			Type: "text",
-		},
-		{
-			Name: "span.attributes.http.method.not_keyword",
-			Type: "keyword",
-		},
-	}
+    tagsMock := []tagsquery.TagInfo{
+        {
+            Name: "span.attributes.http.method.keyword",
+            Type: "keyword",
+        },
+        {
+            Name: "span.attributes.http.method",
+            Type: "text",
+        },
+        {
+            Name: "span.attributes.http.method.not_keyword",
+            Type: "keyword",
+        },
+    }
 
-	tagsResult := removeDuplicatedTextTags(tagsMock)
+    tagsResult := removeDuplicatedTextTags(tagsMock)
 
-	assert.Len(t, tagsResult, 2)
+    assert.Len(t, tagsResult, 2)
 
-	var tagsNames []string
-	for _, tag := range tagsResult {
-		tagsNames = append(tagsNames, tag.Name)
-	}
+    var tagsNames []string
+    for _, tag := range tagsResult {
+        tagsNames = append(tagsNames, tag.Name)
+    }
 
-	assert.Contains(t, tagsNames, "span.attributes.http.method")
-	assert.Contains(t, tagsNames, "span.attributes.http.method.not_keyword")
+    assert.Contains(t, tagsNames, "span.attributes.http.method")
+    assert.Contains(t, tagsNames, "span.attributes.http.method.not_keyword")
 }
 
 func Test_BuildTagsValuesRequest_sanity(t *testing.T) {
-	expectedJson := `{
+    expectedJson := `{
  "aggregations": {
   "scope.name": {
    "terms": {
@@ -228,14 +228,14 @@ func Test_BuildTagsValuesRequest_sanity(t *testing.T) {
     {
  	"range": {
  	 "span.startTimeUnixNano": {
- 	  "gte": 1669194382741000000
+ 	  "gte": 1669194382741
  	 }
  	}
     },
     {
  	"range": {
  	 "span.endTimeUnixNano": {
- 	  "lte": 1669799182741000000
+ 	  "lte": 1669799182741
  	 }
  	}
     }
@@ -244,34 +244,34 @@ func Test_BuildTagsValuesRequest_sanity(t *testing.T) {
  },
  "size": 0
 }`
-	tagsMapping := []tagsquery.TagInfo{
-		{
-			Name: "scope.name",
-			Type: "text",
-		},
-	}
-	filter := model.KeyValueFilter{
-		Key:      "resource.attributes.service.name",
-		Operator: "in",
-		Value:    []string{"demo-server"},
-	}
-	request := tagsquery.TagValuesRequest{
-		Timeframe: &model.Timeframe{StartTime: 1669194382741000000, EndTime: 1669799182741000000},
-		SearchFilters: []model.SearchFilter{
-			{
-				KeyValueFilter: &filter,
-			},
-		},
-	}
-	res, err := buildTagsValuesRequest(request, tagsMapping)
-	assert.Nil(t, err)
-	j, err := json.Marshal(res)
-	assert.Nil(t, err)
-	assert.JSONEq(t, expectedJson, fmt.Sprintf("%+v", string(j)))
+    tagsMapping := []tagsquery.TagInfo{
+        {
+            Name: "scope.name",
+            Type: "text",
+        },
+    }
+    filter := model.KeyValueFilter{
+        Key:      "resource.attributes.service.name",
+        Operator: "in",
+        Value:    []string{"demo-server"},
+    }
+    request := tagsquery.TagValuesRequest{
+        Timeframe: &model.Timeframe{StartTime: 1669194382741000000, EndTime: 1669799182741000000},
+        SearchFilters: []model.SearchFilter{
+            {
+                KeyValueFilter: &filter,
+            },
+        },
+    }
+    res, err := buildTagsValuesRequest(request, tagsMapping)
+    assert.Nil(t, err)
+    j, err := json.Marshal(res)
+    assert.Nil(t, err)
+    assert.JSONEq(t, expectedJson, fmt.Sprintf("%+v", string(j)))
 }
 
 func Test_BuildTagsValuesRequest_BuildWithoutTimeframe(t *testing.T) {
-	expectedJson := `{
+    expectedJson := `{
  "aggregations": {
   "scope.name": {
    "terms": {
@@ -301,28 +301,28 @@ func Test_BuildTagsValuesRequest_BuildWithoutTimeframe(t *testing.T) {
  },
  "size": 0
 }`
-	tagsMapping := []tagsquery.TagInfo{
-		{
-			Name: "scope.name",
-			Type: "text",
-		},
-	}
-	filter := model.KeyValueFilter{
-		Key:      "resource.attributes.service.name",
-		Operator: "in",
-		Value:    []string{"demo-server"},
-	}
-	request := tagsquery.TagValuesRequest{
-		Timeframe: nil,
-		SearchFilters: []model.SearchFilter{
-			{
-				KeyValueFilter: &filter,
-			},
-		},
-	}
-	res, err := buildTagsValuesRequest(request, tagsMapping)
-	assert.Nil(t, err)
-	j, err := json.Marshal(res)
-	assert.Nil(t, err)
-	assert.JSONEq(t, expectedJson, fmt.Sprintf("%+v", string(j)))
+    tagsMapping := []tagsquery.TagInfo{
+        {
+            Name: "scope.name",
+            Type: "text",
+        },
+    }
+    filter := model.KeyValueFilter{
+        Key:      "resource.attributes.service.name",
+        Operator: "in",
+        Value:    []string{"demo-server"},
+    }
+    request := tagsquery.TagValuesRequest{
+        Timeframe: nil,
+        SearchFilters: []model.SearchFilter{
+            {
+                KeyValueFilter: &filter,
+            },
+        },
+    }
+    res, err := buildTagsValuesRequest(request, tagsMapping)
+    assert.Nil(t, err)
+    j, err := json.Marshal(res)
+    assert.Nil(t, err)
+    assert.JSONEq(t, expectedJson, fmt.Sprintf("%+v", string(j)))
 }
