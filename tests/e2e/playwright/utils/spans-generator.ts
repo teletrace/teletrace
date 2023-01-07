5c60b167cc4a3f583c54bfb593abed3740cbcb2e
Reut Baruch <reutbar@cisco.com>
2023-01-07 19:22:27 +0200

    create trace using openTelemetry API

---
+++ tests/e2e/playwright/utils/spans-generator.ts
@@ -0,0 +1,102 @@
+import * as opentelemetry from "@opentelemetry/api";
+import { Span } from "@opentelemetry/api";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import {
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+type traceProps = {
+  serviceName: string;
+  traceName: string;
+  mainSpanName: string;
+  numOfChildSpans: number;
+  childSpansName: string;
+};
+
+export function createMultipleTraces(traceProps: traceProps[]) {
+  console.log(traceProps.length);
+  if (traceProps.length === 0) {
+    return;
+  }
+
+  const parent = createOpenTelemetryTrace(traceProps[0], null);
+
+  for (let i = 1; i < traceProps.length; i += 1) {
+    createOpenTelemetryTrace(traceProps[i], parent);
+  }
+}
+
+function createOpenTelemetryTrace(
+  traceProps: traceProps,
+  parentSpan: Span | null
+) {
+  const provider = new BasicTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: traceProps.serviceName,
+    }),
+  });
+
+  const exporter = new OTLPTraceExporter();
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+  provider.register();
+
+  const tracer = provider.getTracer(traceProps.traceName);
+
+  if (!parentSpan) {
+    parentSpan = tracer.startSpan(traceProps.mainSpanName);
+  }
+
+  //   parentSpan.setAttribute("key", "value");
+
+  for (let i = 0; i < traceProps.numOfChildSpans; i += 1) {
+    doWork(traceProps.childSpansName, parentSpan, tracer);
+  }
+
+  parentSpan.end();
+  exporter.shutdown();
+
+  return parentSpan;
+}
+
+function doWork(
+  childSpansName: string,
+  parent: opentelemetry.Span,
+  tracer: opentelemetry.Tracer
+) {
+  const ctx = opentelemetry.trace.setSpan(
+    opentelemetry.context.active(),
+    parent
+  );
+  const span = tracer.startSpan(childSpansName, undefined, ctx);
+
+  // simulate some random work.
+  for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {}
+
+  span.setAttribute("key", "value");
+
+  span.addEvent("invoking doWork");
+
+  span.end();
+}
+
+let l: traceProps[] = [];
+
+for (let i = 0; i < 4; i += 1) {
+  const t: traceProps = {
+    serviceName: "server-" + i,
+    traceName: "trace-" + i,
+    mainSpanName: "span" + i,
+    numOfChildSpans: 2,
+    childSpansName: "child" + i,
+  };
+
+  l.push(t);
+}
+
+createMultipleTraces(l);
