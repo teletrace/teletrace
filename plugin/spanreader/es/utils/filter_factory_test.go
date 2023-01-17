284113488eb449fd343a89d452f31f41cb714655
Nir Feinblat <92082083+nirf-cisco@users.noreply.github.com>
2023-01-17 15:10:27 +0200

    fix: elastic "not must" filters are overridden by the "must" filters  (#1204)
    ## What this PR does:
    Elastic query is wrong in 2 cases:
    1. there are multiple "must not" (not_in for instance) filters
    2. a must filter becomes before a must not filter
    ## Which issue(s) this PR fixes:
    
    Fixes #1193
    
    ## Checklist
    
    - [x] Tests updated
    - [ ] Documentation added
    - [ ] `CHANGELOG.md` updated - the order of entries should be `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`

---
+++ plugin/spanreader/es/utils/filter_factory_test.go
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2022 Cisco Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import (
+	"encoding/json"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
+	"github.com/stretchr/testify/assert"
+	"oss-tracing/pkg/model"
+	"testing"
+)
+
+func TestMustAndMustNotFilters(t *testing.T) {
+	expectedJson := `{
+	"bool": {
+		"must": [
+			{
+				"bool": {
+					"should": [
+						{
+							"match_phrase": {
+								"resource.attributes.service.name": {
+									"query": "demo-server"
+								}
+							}
+						}
+					]
+				}
+			}
+		],
+		"must_not": [
+			{
+				"bool": {
+					"should": [
+						{
+							"match_phrase": {
+								"span.name": {
+									"query": "ExecuteRequest"
+								}
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+}`
+	query := types.NewQueryContainerBuilder()
+	kvFilters := []model.KeyValueFilter{
+		{Key: "resource.attributes.service.name",
+			Operator: "in",
+			Value:    []string{"demo-server"},
+		},
+		{Key: "span.name",
+			Operator: "not_in",
+			Value:    []string{"ExecuteRequest"},
+		},
+	}
+
+	query, err := BuildFilters(query, kvFilters, WithMiliSecTimestampAsNanoSec())
+	assert.Nil(t, err)
+	queryJson, err := json.Marshal(query.Build())
+	assert.Nil(t, err)
+	assert.JSONEq(t, expectedJson, string(queryJson))
+	//validate that filter orders doesn't effect
+	query2 := types.NewQueryContainerBuilder()
+	kvFilters2 := []model.KeyValueFilter{
+		{Key: "span.name",
+			Operator: "not_in",
+			Value:    []string{"ExecuteRequest"},
+		},
+		{Key: "resource.attributes.service.name",
+			Operator: "in",
+			Value:    []string{"demo-server"},
+		},
+	}
+
+	query2, err = BuildFilters(query2, kvFilters2, WithMiliSecTimestampAsNanoSec())
+	assert.Nil(t, err)
+	queryJson2, err := json.Marshal(query2.Build())
+	assert.Nil(t, err)
+	assert.JSONEq(t, expectedJson, string(queryJson2))
+}
+
+func TestMultiMustNotFilters(t *testing.T) {
+	expectedJson := `{
+	"bool": {
+		"must_not": [
+			{
+				"bool": {
+					"should": [
+						{
+							"match_phrase": {
+								"resource.attributes.service.name": {
+									"query": "demo-server"
+								}
+							}
+						}
+					]
+				}
+			},
+			{
+				"bool": {
+					"should": [
+						{
+							"match_phrase": {
+								"span.name": {
+									"query": "ExecuteRequest"
+								}
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+}`
+	query := types.NewQueryContainerBuilder()
+	kvFilters := []model.KeyValueFilter{
+		{Key: "resource.attributes.service.name",
+			Operator: "not_in",
+			Value:    []string{"demo-server"},
+		},
+		{Key: "span.name",
+			Operator: "not_in",
+			Value:    []string{"ExecuteRequest"},
+		},
+	}
+
+	query, err := BuildFilters(query, kvFilters, WithMiliSecTimestampAsNanoSec())
+	assert.Nil(t, err)
+	queryJson, err := json.Marshal(query.Build())
+	assert.Nil(t, err)
+	assert.JSONEq(t, expectedJson, string(queryJson))
+}
