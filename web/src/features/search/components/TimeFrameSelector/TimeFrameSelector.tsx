d6cbd803f94358309aaf744d231a314bbddb9274
Peleg Naohr <pelegnahor@gmail.com>
2023-01-24 10:05:23 +0200

    custom title doesn't change when a range applied

--- web/src/features/search/components/TimeFrameSelector/TimeFrameSelector.tsx
+++ web/src/features/search/components/TimeFrameSelector/TimeFrameSelector.tsx
@@ -118,7 +118,8 @@ export const TimeFrameSelector = () => {
           key={customOption.label}
         >
           <CalendarTodayOutlined sx={{ paddingRight: "7px" }} />
-          {isSelected?.label === customOption?.label && !open
+          {isSelected?.label === customOption?.label &&
+          isSelected.label === customOption.label
             ? getFormattedCustomTimeframe()
             : customOption.label}
         </ToggleButton>
