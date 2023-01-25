09789de91f2b23d1aa065679f0138f084b861e15
Reut Baruch <89512916+BaruchReut@users.noreply.github.com>
2023-01-25 18:01:40 +0200

    feat(trace-view): add parent id to spans' details list (#1293)
    ## What this PR does:
    Adds parent Id to spans' details list
    
    ## Which issue(s) this PR fixes:
    Fixes #1126
    
    ## Example:
    span-2 is the child of span-1
    
    ![image](https://user-images.githubusercontent.com/89512916/214572291-9f577401-a60d-46cb-b1f9-98833b73cbea.png)
    
    ![image](https://user-images.githubusercontent.com/89512916/214572581-811e224f-b1a5-4242-861d-42c4a1c311c6.png)

--- web/src/features/trace/components/SpanDetailsList/SpanDetails/SpanDetails.tsx
+++ web/src/features/trace/components/SpanDetailsList/SpanDetails/SpanDetails.tsx
@@ -42,7 +42,7 @@ export interface SpanDetailsProps {
 }
 
 function getBasicAttributes(span: InternalSpan): Attributes {
-  return {
+  const returnAttributes: Attributes = {
     service_name: span.resource.attributes["service.name"],
     name: span.span.name,
     status: span.span.status.code,
@@ -52,6 +52,9 @@ function getBasicAttributes(span: InternalSpan): Attributes {
     span_id: span.span.spanId,
     trace_id: span.span.traceId,
   };
+  span.span.parentSpanId &&
+    (returnAttributes["parent_span_id"] = span.span.parentSpanId);
+  return returnAttributes;
 }
 
 export const SpanDetails = ({ span, expanded, onChange }: SpanDetailsProps) => {
