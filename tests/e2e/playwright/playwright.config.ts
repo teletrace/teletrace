3e161dac2e481ece3d70161d38e1e2f82c545c58
Reut Baruch <reutbar@cisco.com>
2023-02-01 15:14:44 +0200

    update workers only in CI

--- tests/e2e/playwright/playwright.config.ts
+++ tests/e2e/playwright/playwright.config.ts
@@ -52,7 +52,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 3,
+  workers: process.env.CI ? 3 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
