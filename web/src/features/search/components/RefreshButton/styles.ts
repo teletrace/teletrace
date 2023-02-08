2ea314bcb45847b6df39fed4d701afaaf0aaee61
Haim Raitsev <haimrait@cisco.com>
2023-02-08 10:56:19 +0200

    fix: refresh component change icon color and animation (#1425)
    ## What this PR does:
    Changes live spans animation, refresh button color
    
    ## Which issue(s) this PR fixes:
    Fixes #1300 
    
    ![Screenshot 2023-02-08 at 10 29 47](https://user-images.githubusercontent.com/21267310/217476941-c4a834a6-975b-4e50-9e96-6dad628c344b.png)

--- web/src/features/search/components/RefreshButton/styles.ts
+++ web/src/features/search/components/RefreshButton/styles.ts
@@ -20,37 +20,41 @@ const breathingAnimation = keyframes`
   0% {
       box-shadow:
       0 0 0 1px ${liveSpansIconTransparentColor},
-      0 0 0 2px ${liveSpansIconTransparentColor},
-      0 0 0 3px ${liveSpansIconTransparentColor}
+      0 0 0 2px ${liveSpansIconTransparentColor}
   }
  50% {
       box-shadow:
-      0 0 0 2px ${liveSpansIconTransparentColor},
       0 0 0 4px ${liveSpansIconTransparentColor},
-      0 0 0 6px ${liveSpansIconTransparentColor}
+      0 0 0 8px ${liveSpansIconTransparentColor}
  }
  100% {
       box-shadow:
       0 0 0 1px ${liveSpansIconTransparentColor},
-      0 0 0 2px ${liveSpansIconTransparentColor},
-      0 0 0 3px ${liveSpansIconTransparentColor}
+      0 0 0 2px ${liveSpansIconTransparentColor}
   }
 `;
 
 const styles = {
   container: { alignItems: "center", height: "40px" },
   iconWrapper: { width: "30px", justifyContent: "center" },
+  liveSpansIconWrapper: {
+    height: "30px",
+    width: "30px",
+    fontSize: "28px",
+  },
   liveSpansIcon: {
     color: "transparent",
     background: "rgba(0, 205, 231, 1)",
     borderRadius: "50%",
-    maxHeight: "8px",
-    maxWidth: "8px",
-    minHeight: "8px",
-    minWidth: "8px",
+    maxHeight: "10px",
+    maxWidth: "10px",
+    minHeight: "10px",
+    minWidth: "10px",
     top: "50%",
-    marginBottom: "2px",
-    animation: `${breathingAnimation} 4s linear infinite`,
+    animation: `${breathingAnimation} 2s linear infinite`,
+  },
+  refreshIcon: {
+    color: "#00cde7",
   },
   refreshingIcon: {
     maxHeight: "17px",
@@ -59,7 +63,7 @@ const styles = {
     minWidth: "17px",
   },
   refreshStatusText: {
-    marginLeft: "5px",
+    marginLeft: "3px",
     fontSize: "14px",
     color: "#B6B7BE",
     fontFamily: "inter",
