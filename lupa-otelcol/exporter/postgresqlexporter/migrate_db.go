a0055fe7b659794c2d3a89bb2f93a737e046a9a2
Suha <sedris@cisco.com>
2023-02-12 13:31:14 +0200

    add schema migration to posgresql exporter, using blank up and down files

---
+++ lupa-otelcol/exporter/postgresqlexporter/migrate_db.go
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2022 Cisco Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package postgresqlexporter
+
+import (
+	"embed"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+)
+
+//go:embed migrations
+var migrations embed.FS
+
+func migrateSchema(dbName string) error {
+	driver, err := iofs.New(migrations, "migrations")
+	if err != nil {
+		return fmt.Errorf("could not read db migrations: %+v", err)
+	}
+
+	m, err := migrate.NewWithSourceInstance(
+		"iofs", driver, fmt.Sprintf("postgresql://user:password@localhost:5432/%s?sslmode=disable", dbName))
+	if err != nil {
+		return fmt.Errorf("could not create migrate instance: %+v", err)
+	}
+
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("could not migrate db: %+v", err)
+	}
+
+	return nil
+}
