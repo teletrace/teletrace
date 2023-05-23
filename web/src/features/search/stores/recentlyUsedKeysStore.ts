8cb699159bacf4a27c68108d711c7dc2bc220725
Suha <sedris@cisco.com>
2023-05-23 12:20:05 +0300

    fix a bug reverse() was causing

--- web/src/features/search/stores/recentlyUsedKeysStore.ts
+++ web/src/features/search/stores/recentlyUsedKeysStore.ts
@@ -26,6 +26,7 @@ interface RecentlyUsedKeysSlice {
     };
   }
   
+  const MAX_RECENTLY_USED_KEYS = 3;
   const createRecentlyUsedKeysSlice: StateCreator<
   RecentlyUsedKeysSlice,
   [],
@@ -35,7 +36,7 @@ interface RecentlyUsedKeysSlice {
     recentlyUsedKeys: [],
     addRecentlyUsedKey: (tag: AvailableTag) => {
       set((state: RecentlyUsedKeysSlice) => {
-        const updatedKeys = [...state.recentlyUsedKeysState.recentlyUsedKeys, tag].slice(-3).reverse();
+        const updatedKeys = [tag, ...state.recentlyUsedKeysState.recentlyUsedKeys.filter(key => key !== tag)].slice(0,MAX_RECENTLY_USED_KEYS);
         return {
           recentlyUsedKeysState: {
             ...state.recentlyUsedKeysState,
